3c529a438d1d2b84eadfc79cae9804c5f28ff614
庄建国 <9340100@qq.com>
2022-06-27 13:16:41 +0000

    amis

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, Script, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
 
---
+++ src/pages/app/[app_id]/[tab_id].jsx
@@ -0,0 +1,59 @@
+import dynamic from 'next/dynamic'
+import Document, { Script, Head, Main, NextScript } from 'next/document'
+import React, { useState, useEffect } from 'react';
+
+export default function Page ({schema}) {
+
+  useEffect(() => {
+    (function () {
+      let amis = amisRequire('amis/embed');
+      let amisScoped = amis.embed('#amis-root', 
+        schema, 
+        {}, 
+        {
+          theme: 'antd'
+        }
+      );
+    })();
+  });
+
+  return (
+    <>
+      {/* <link rel="stylesheet" href="https://unpkg.com/amis@2.0.0-rc.20/lib/themes/cxd.css" /> */}
+      <link rel="stylesheet" href="https://unpkg.com/amis@2.0.0-rc.20/lib/themes/antd.css" />
+      <link rel="stylesheet" href="https://unpkg.com/amis@2.0.0-rc.20/lib/themes/helper.css" />
+      <link rel="stylesheet" href="https://unpkg.com/amis@2.0.0-rc.20/sdk/iconfont.css" />
+      <script src="https://unpkg.com/amis@2.0.0-rc.20/sdk/sdk.js"></script>
+      <div id="amis-root" className="app-wrapper"></div>
+    </>
+  )
+}
+
+
+
+export async function getServerSideProps(context) {
+  const schema = {
+    type: 'page',
+    title: '表单页面',
+    body: {
+      type: 'form',
+      mode: 'horizontal',
+      api: '/saveForm',
+      body: [
+        {
+          label: 'Name',
+          type: 'input-text',
+          name: 'name'
+        },
+        {
+          label: 'Email',
+          type: 'input-email',
+          name: 'email'
+        }
+      ]
+    }
+  };
+  return {
+    props: { schema },
+  }
+}
