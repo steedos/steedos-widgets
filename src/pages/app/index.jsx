2d4f8ba31dd622e32db41951a29706d512149755
庄建国 <9340100@qq.com>
2022-06-28 08:41:35 +0000

    callbackUrl

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -18,7 +18,7 @@ export async function getServerSideProps(context) {
   if (!session) {
     return {
       redirect: {
-        destination: '/login',
+        destination: '/login?callbackUrl=/app',
         permanent: false,
       },
     }
