53423713f3b1089e9088040b8b494db84f0aad3b
庄建国 <9340100@qq.com>
2022-08-02 10:39:44 +0000

    listview

--- src/pages/app/[app_id]/[tab_id]/index.jsx
+++ src/pages/app/[app_id]/[tab_id]/index.jsx
@@ -82,7 +82,7 @@ export default function Page (props) {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                       >
-                        <Listbox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <Listbox.Options className="z-50 absolute mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                           {values(schema?.uiSchema?.list_views).map((listView, personIdx) => (
                                 <Listbox.Option key={personIdx} value={listView} className={({ active }) =>
                                 `relative cursor-default select-none py-2 pl-10 pr-4 ${
