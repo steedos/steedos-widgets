a2447a4bcf55ff77864d457cf1a05a6a292723d5
庄建国 <9340100@qq.com>
2022-08-02 10:30:47 +0000

    Listview

--- src/pages/app/[app_id]/[tab_id]/index.jsx
+++ src/pages/app/[app_id]/[tab_id]/index.jsx
@@ -48,59 +48,161 @@ export default function Page (props) {
   }, [tab_id, selectedListView, formFactor]);
   return (
     <>
-      <div className="relative z-9 sm:pb-0 border-b sm:border-b-0">
+  <div className="slds-page-header">
+    <div className="slds-page-header__row">
+      <div className="slds-page-header__col-title">
+        <div className="slds-media">
+          <div className="slds-media__figure">
+            <span className="slds-icon_container slds-icon-standard-opportunity">
+              <svg className="slds-icon slds-page-header__icon" aria-hidden="true">
+                <use xlinkHref="/assets/icons/standard-sprite/svg/symbols.svg#opportunity"></use>
+              </svg>
+            </span>
+          </div>
+          <div className="slds-media__body">
+            <div className="slds-page-header__name">
+              <div className="slds-page-header__name-title">
+                <div>
+                  <span>{schema?.uiSchema?.label}</span>
+
+                  <Listbox value={selectedListView} onChange={setSelectedListView}>
+                    <div className="relative mt-1 w-[1/2]">
+                      <Listbox.Button className="relative w-full cursor-default pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                        <span className="slds-page-header__title slds-truncate">{selectedListView?.label || schema?.uiSchema?.list_views.all?.label}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                          <SelectorIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </Listbox.Button>
+                      <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Listbox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          {values(schema?.uiSchema?.list_views).map((listView, personIdx) => (
+                                <Listbox.Option key={personIdx} value={listView} className={({ active }) =>
+                                `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                  active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                }`
+                              }>
+                                
+                                    <span
+                                      className={`block truncate ${
+                                        (selectedListView?.name ? selectedListView.name : 'all') === listView.name ? 'font-medium' : 'font-normal'
+                                      }`}
+                                    >
+                                      {listView.label}
+                                    </span>
+                                    {(selectedListView?.name ? selectedListView.name : 'all') === listView.name ? (
+                                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                      </span>
+                                    ) : null}
+                                </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                        </Transition>
+                    </div>
+                    
+                  </Listbox>
+                </div>
+
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="slds-page-header__col-actions">
+        <div className="slds-page-header__controls">
+          <div className="slds-page-header__control">
+            
+            <ListButtons app_id={app_id} tab_id={tab_id} schema={schema}></ListButtons>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="slds-page-header__row">
+      <div className="slds-page-header__col-meta">
+        <p className="slds-page-header__meta-text">10 items • Updated 13 minutes ago</p>
+      </div>
+      <div className="slds-page-header__col-controls">
+        <div className="slds-page-header__controls">
+          <div className="slds-page-header__control">
+            <div className="slds-dropdown-trigger slds-dropdown-trigger_click">
+              <button className="slds-button slds-button_icon slds-button_icon-more" aria-haspopup="true" aria-expanded="false" title="List View Controls">
+                <svg className="slds-button__icon" aria-hidden="true">
+                  <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#settings"></use>
+                </svg>
+                <svg className="slds-button__icon slds-button__icon_x-small" aria-hidden="true">
+                  <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#down"></use>
+                </svg>
+                <span className="slds-assistive-text">List View Controls</span>
+              </button>
+            </div>
+          </div>
+          <div className="slds-page-header__control">
+            <div className="slds-dropdown-trigger slds-dropdown-trigger_click">
+              <button className="slds-button slds-button_icon slds-button_icon-more" aria-haspopup="true" aria-expanded="false" title="Change view">
+                <svg className="slds-button__icon" aria-hidden="true">
+                  <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#table"></use>
+                </svg>
+                <svg className="slds-button__icon slds-button__icon_x-small" aria-hidden="true">
+                  <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#down"></use>
+                </svg>
+                <span className="slds-assistive-text">Change view</span>
+              </button>
+            </div>
+          </div>
+          <div className="slds-page-header__control">
+            <button className="slds-button slds-button_icon slds-button_icon-border-filled" title="Edit List">
+              <svg className="slds-button__icon" aria-hidden="true">
+                <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#edit"></use>
+              </svg>
+              <span className="slds-assistive-text">Edit List</span>
+            </button>
+          </div>
+          <div className="slds-page-header__control">
+            <button className="slds-button slds-button_icon slds-button_icon-border-filled" title="Refresh List">
+              <svg className="slds-button__icon" aria-hidden="true">
+                <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#refresh"></use>
+              </svg>
+              <span className="slds-assistive-text">Refresh List</span>
+            </button>
+          </div>
+          <div className="slds-page-header__control">
+            <ul className="slds-button-group-list">
+              <li>
+                <button className="slds-button slds-button_icon slds-button_icon-border-filled" title="Charts">
+                  <svg className="slds-button__icon" aria-hidden="true">
+                    <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#chart"></use>
+                  </svg>
+                  <span className="slds-assistive-text">Charts</span>
+                </button>
+              </li>
+              <li>
+                <button className="slds-button slds-button_icon slds-button_icon-border-filled" title="Filters">
+                  <svg className="slds-button__icon" aria-hidden="true">
+                    <use xlinkHref="/assets/icons/utility-sprite/svg/symbols.svg#filterList"></use>
+                  </svg>
+                  <span className="slds-assistive-text">Filters</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+      <div className="mt-6 relative z-9 sm:pb-0 border-b sm:border-b-0">
               <div className="relative space-y-4">
                   <div className="relative pointer-events-auto w-full px-3 text-[0.8125rem] leading-5">
-                      <div className="flex justify-between">
-                          <div className="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200">{schema?.uiSchema?.label}</div>
-                          <div className="flex flex-nowrap space-x-2 ml-6 fill-slate-400">
-                            <ListButtons app_id={app_id} tab_id={tab_id} schema={schema}></ListButtons>
-                          </div>
-                      </div>
-                      <Listbox value={selectedListView} onChange={setSelectedListView}>
-                        <div className="relative mt-1 w-[1/2]">
-                          <Listbox.Button className="relative w-full cursor-default py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                            <span className="block truncate">{selectedListView?.label || schema?.uiSchema?.list_views.all?.label}</span>
-                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                              <SelectorIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Listbox.Button>
-                          <Transition
-                            as={Fragment}
-                            leave="transition ease-in duration-100"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                          >
-                            <Listbox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                              {values(schema?.uiSchema?.list_views).map((listView, personIdx) => (
-                                    <Listbox.Option key={personIdx} value={listView} className={({ active }) =>
-                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                                    }`
-                                  }>
-                                   
-                                        <span
-                                          className={`block truncate ${
-                                            (selectedListView?.name ? selectedListView.name : 'all') === listView.name ? 'font-medium' : 'font-normal'
-                                          }`}
-                                        >
-                                          {listView.label}
-                                        </span>
-                                        {(selectedListView?.name ? selectedListView.name : 'all') === listView.name ? (
-                                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                          </span>
-                                        ) : null}
-                                    </Listbox.Option>
-                              ))}
-                            </Listbox.Options>
-                            </Transition>
-                        </div>
-                        
-                      </Listbox>
+
                       
                   </div>
               </div>
