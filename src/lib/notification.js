1cec3da0773500d7a5ab07a9435b5fd119947d58
baozhoutao <baozhoutao@hotoa.com>
2022-08-15 13:51:51 +0800

    Update notification.js

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -1,12 +1,13 @@
-import { fetchAPI } from "./steedos.client"
+import { fetchAPI, getSteedosAuth } from "./steedos.client"
 
 export const getNotifications = async () => {
+    const auth = getSteedosAuth();
     const query = `
     {
-        notifications(filters: ["owner","=","62538e715ab244718b22348b"], sort: "created desc,name", top : 10){
+        notifications(filters: ["owner","=","${auth.userId}"], sort: "created desc,name", top : 10){
           name,body,related_to,related_name,url,owner,is_read,from,created
         },
-        unReadCount: notifications__count(filters: [["owner","=","62538e715ab244718b22348b"], ["is_read", "!=", true]])
+        unReadCount: notifications__count(filters: [["owner","=","${auth.userId}"], ["is_read", "!=", true]])
     }
     `;
     const result = await fetchAPI('/graphql', {
