f2047e4dee7c658a8fd419e1173c2e18ae82106a
庄建国 <zhuangjianguo@steedos.com>
2022-08-23 04:18:15 +0000

    sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     fontSize: {
       xs: ['0.75rem', { lineHeight: '1rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      tiny: ['0.925rem', { lineHeight: '1.5rem' }],
       base: ['1rem', { lineHeight: '1.75rem' }],
       lg: ['1.125rem', { lineHeight: '2rem' }],
       xl: ['1.25rem', { lineHeight: '2rem' }],
