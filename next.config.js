3941f29b614e2407bea9e28e13eb44ed46365119
baozhoutao <baozhoutao@hotoa.com>
2022-08-25 09:58:04 +0800

    fix bug

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-07-20 16:29:22
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-08-15 10:47:18
+ * @LastEditTime: 2022-08-25 09:50:36
  * @Description: 
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'steedos-ee/packages/experience/.next',
+  // distDir: 'steedos-ee/packages/experience/.next',
   reactStrictMode: false, // 不要开启. amis 不支持 strict mode
   experimental: {
     images: {
