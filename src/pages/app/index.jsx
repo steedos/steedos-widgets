aa83db655966417cdc382271b83fe58c017c8802
庄建国 <zhuangjianguo@steedos.com>
2022-08-25 07:25:47 +0000

    app loading

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -5,12 +5,9 @@
  * @LastEditTime: 2022-08-23 15:15:34
  * @Description: 
  */
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
-import { AppLauncher } from '@/components/AppLauncher'
-import { LauncherLayout } from "@/components/LauncherLayout";
+import { Loading } from '@/components/Loading'
 
 import { getApps } from '@/lib/apps'
 
@@ -26,26 +23,6 @@ export default function Apps() {
   }, [])
 
   return (
-    <>
-    {/* <AppLauncher router={router}></AppLauncher> */}
-  </>
+    <Loading/>
   )
-}
-Apps.getLayout = function getLayout(page) {
-  return LauncherLayout;
-};
-export async function getServerSideProps(context) {
-  const session = context.req.session || await unstable_getServerSession(context.req, context.res, authOptions)
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login?callbackUrl=/app',
-        permanent: false,
-      },
-    }
-  }
-  return {
-    props: { },
-  }
 }
