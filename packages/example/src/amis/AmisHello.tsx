f3fd4ff7f7d241974a5f19cbc7a194cc7c5532ef
庄建国 <zhuangjianguo@steedos.com>
2022-08-31 09:03:31 +0000

    AmisHello

--- packages/example/src/amis/AmisHello.tsx
+++ packages/example/src/amis/AmisHello.tsx
@@ -1,10 +1,28 @@
 export const AmisHello = ({title, body}) => {
   return {
-    "type": "page",
-    "body": "Hello ${title}"
+    "type": "service",
+    "body": [
+      {
+        "type": "panel",
+        "title": "Hello ${title}",
+        "body": [
+          {
+            "type": "tpl",
+            "tpl": "内容",
+            "inline": false,
+            "id": "u:43e9c9279437"
+          }
+        ],
+        "id": "u:75671fc7430c"
+      }
+    ],
+    "id": "u:73601cf76024",
+    "data": {
+      "title": "World"
+    }
   }
 }
 
 AmisHello.meta = {
-  
+
 }
