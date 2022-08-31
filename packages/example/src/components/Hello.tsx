fb49e5732bef66c881f4d03894411459bea2d6f7
庄建国 <zhuangjianguo@steedos.com>
2022-08-31 08:45:00 +0000

    Hello Render Body

--- packages/example/src/components/Hello.tsx
+++ packages/example/src/components/Hello.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 import './Hello.less'
 
-export const Hello = (props: any) => {
-  return (<div className='example-hello'>
-    <div>Hello World！{props.title}</div>
-    <div>{props.content}</div>
+export const Hello = ({title = 'World', body, render}) => {
+  return (
+  <div className='example-hello'>
+    <div>Hello {title}!</div>
+    {body ? (
+      <div className="container">
+        {render('body', body, {
+          // 这里的信息会作为 props 传递给子组件，一般情况下都不需要这个
+        })}
+      </div>
+    ) : null}
   </div>)
 }
