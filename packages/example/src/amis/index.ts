3c0f9f2829aafd7174830bc0c0d76dfaf72e1fcd
baozhoutao <baozhoutao@hotoa.com>
2022-09-02 11:01:01 +0800

    添加amis schema form 、amis schema listview 测试组件

--- packages/example/src/amis/index.ts
+++ packages/example/src/amis/index.ts
@@ -1,1 +1,10 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-08-31 16:32:35
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-09-01 18:42:03
+ * @Description: 
+ */
 export * from './AmisHello';
+export * from './AmisSteedosObjectForm';
+export * from './AmisObjectListview';
--- packages/example/src/meta.ts
+++ packages/example/src/meta.ts
@@ -1,16 +1,23 @@
-import Hello from "./metas/Hello";
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-08-31 16:32:35
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-09-01 18:46:29
+ * @Description: 
+ */
+// import Hello from "./metas/Hello";
 import AmisHello from "./metas/AmisHello";
-
-const components = [Hello, AmisHello];
-const componentList = [
-  {
-    title: "哈喽组件",
-    icon: "",
-    children: [Hello]
-  }
-];
-
+import AmisSteedosObjectForm from "./metas/AmisSteedosObjectForm";
+import AmisObjectListview from "./metas/AmisObjectListview";
+const components = [AmisHello, AmisSteedosObjectForm, AmisObjectListview];
+// const componentList = [
+//   {
+//     title: "哈喽组件",
+//     icon: "",
+//     children: [Hello]
+//   }
+// ];
 export default {
-  componentList,
+  // componentList,
   components
 };
