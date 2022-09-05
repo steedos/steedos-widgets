743e19f671a13fc4e98a4fb5fcf5f89e7d090cda
庄建国 <zhuangjianguo@steedos.com>
2022-09-05 10:15:05 +0000

    dnd-kit

---
+++ packages/dnd-kit/tsdx.config.js
@@ -0,0 +1,13 @@
+const postcss = require('rollup-plugin-postcss');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        inject: true, // 这里改为了 true
+        extract: !!options.writeMeta,
+      }),
+    );
+    return config;
+  },
+}
