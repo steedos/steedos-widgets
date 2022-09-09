995ba0522bfdf567a3457ba412969a63e7328f86
baozhoutao <baozhoutao@hotoa.com>
2022-09-09 13:39:22 +0800

    fix: amis render storie error

--- stories/amis-object.stories.tsx
+++ stories/amis-object.stories.tsx
@@ -1,27 +1,69 @@
-import React, {useEffect} from 'react';
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-09-09 11:54:45
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-09-09 13:38:19
+ * @Description: 
+ */
+import React, {useEffect, useState} from 'react';
 
 const AmisRender = ({json})=> {
-  useEffect({
-    amis = amisRequire('amis/embed');
-    let amisScoped = amis.embed('#amis-root', json);
-  })
+  useEffect(()=>{
+    const amis = (window as any).amisRequire('amis/embed');
+    let amisScoped = (amis as any).embed('#amis-root', json);
+    console.log('amisScoped', amisScoped)
+  }, [])
   return (
   <>
-    <head>
-      <script src="https://unpkg.com/amis/sdk/sdk.js"></script>
-      <script src="https://unpkg.com/lodash/lodash.min.js"></script>
-      <link rel="stylesheet" href="https://unpkg.com/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css" />
-      <link rel="stylesheet" href="https://unpkg.com/amis/lib/themes/antd.css" />
-      <link rel="stylesheet" href="https://unpkg.com/amis/lib/helper.css" />
-      <link rel="stylesheet" href="https://unpkg.com/amis/sdk/iconfont.css" />
-      <link rel="stylesheet" href="https://unpkg.com/fontawesome/fontawesome.css" />
-    </head>
-    <div id="#amis-root"/>
+    <div id="amis-root">111</div>
   </>
 )}
 
+const loadJS = async (src)=>{
+  return await new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.onload = () => {
+      resolve(true);
+    };
+    script.src = src;
+    document.body.appendChild(script);
+  })
+}
+const loadCss = async (href)=>{
+  return await new Promise((resolve, reject) => {
+    const link = document.createElement('link');
+    link.setAttribute('href', href);
+    link.setAttribute('rel', 'stylesheet');
+    document.body.appendChild(link);
+    resolve(true);
+  })
+}
+
+
 export default {
-  title: 'Amis Object'
+  title: 'Amis Object',
+  decorators: [(Story)=>{
+    const [isLoaded, setIsLoaded] = useState(false);
+      useEffect(() => {
+        Promise.all([
+          loadJS('https://unpkg.com/amis/sdk/sdk.js'), 
+          loadJS('https://unpkg.com/lodash/lodash.min.js'),
+          loadCss('https://unpkg.com/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css'),
+          loadCss('https://unpkg.com/amis/lib/themes/antd.css'),
+          loadCss('https://unpkg.com/amis/lib/helper.css'),
+          loadCss('https://unpkg.com/amis/sdk/iconfont.css'),
+        ]).then(()=>{
+          setIsLoaded(true)
+        }).catch((error)=>{
+          console.error(error)
+        })
+        return () => {
+          // clean up effects of script here
+        };
+      }, []);
+
+      return isLoaded ? <Story /> : <div>Loading...</div>;
+  }]
 };
 
 
