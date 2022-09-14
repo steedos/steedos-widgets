0f8b11b18188a91d2ff9425f8aa0f454b2664d8a
庄建国 <zhuangjianguo@steedos.com>
2022-09-14 01:39:52 +0000

    storybook: tsconfig-paths-webpack-plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 const path = require('path');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
@@ -45,6 +46,14 @@ module.exports = {
       },
     ];
 
+
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    );
+    
     return config;
   },
 }
