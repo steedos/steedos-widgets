326539b12eb62eebb380d9fad0e70d04cb08de0d
殷亮辉 <yinlianghui@hotoa.com>
2022-10-17 08:44:06 +0000

    移除代码中日志

--- stories/amis.stories.tsx
+++ stories/amis.stories.tsx
@@ -306,76 +306,76 @@ export const AmisSelectUser = () => (
             "multiple": true
           }
         ]
+      },
+      {
+        "type": "form",
+        "mode": "horizontal",
+        "debug": false,
+        "title": "触发事件",
+        "body": [
+          {
+            "type": "tpl",
+            "tpl": "说明：可以配置onEvent属性，触发比如change事件，不支持在设计器中配置",
+          },
+          {
+            "label": "change事件",
+            "type": "steedos-select-user",
+            "name": "admins",
+            "multiple": true,
+            "onEvent": {
+              "change": {
+                "weight": 0,
+                "actions": [
+                  {
+                    "actionType": "custom",
+                    "script": "console.log(\"onEvent change, context.props\", context.props);alert(\"onEvent change\");"
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      },{
+        "type": "form",
+        "mode": "horizontal",
+        "debug": false,
+        "title": "过滤条件",
+        "body": [
+          {
+            "type": "tpl",
+            "tpl": `<div>说明：可以配置filters属性作为选人组件的基本过滤条件，支持传入数组、函数和字符串。</div>
+              <div>当传入函数时，函数参数为field，返回数组格式的过滤条件即可。</div>
+              <div>当传入字符串时，要求字符串格式为:function(field){return [[\"name\", \"contains\", \"三\"]]}。</div>
+              <div>设计器中右侧面板显示为多行文本。</div>
+              `,
+          },
+          {
+            "label": "数组",
+            "type": "steedos-select-user",
+            "name": "admins",
+            "multiple": true,
+            // "filters": [["name", "contains", "王"]],
+          },
+          {
+            "label": "函数",
+            "type": "steedos-select-user",
+            "name": "admins",
+            "multiple": true,
+            "filters": function(field: any){
+              return [["name", "contains", "王"]];
+            },
+          },
+          {
+            "label": "字符串",
+            "type": "steedos-select-user",
+            "name": "admins",
+            "multiple": true,
+            "filters": `function(field){
+              return [["name", "contains", "王"]];
+            }`,
+          }
+        ]
       }
-      // {
-      //   "type": "form",
-      //   "mode": "horizontal",
-      //   "debug": false,
-      //   "title": "触发事件",
-      //   "body": [
-      //     {
-      //       "type": "tpl",
-      //       "tpl": "说明：可以配置onEvent属性，触发比如change事件，不支持在设计器中配置",
-      //     },
-      //     {
-      //       "label": "change事件",
-      //       "type": "steedos-select-user",
-      //       "name": "admins",
-      //       "multiple": true,
-      //       "onEvent": {
-      //         "change": {
-      //           "weight": 0,
-      //           "actions": [
-      //             {
-      //               "actionType": "custom",
-      //               "script": "console.log(\"onEvent change, context.props\", context.props);alert(\"onEvent change\");"
-      //             }
-      //           ]
-      //         }
-      //       }
-      //     }
-      //   ]
-      // },{
-      //   "type": "form",
-      //   "mode": "horizontal",
-      //   "debug": false,
-      //   "title": "过滤条件",
-      //   "body": [
-      //     {
-      //       "type": "tpl",
-      //       "tpl": `<div>说明：可以配置filters属性作为选人组件的基本过滤条件，支持传入数组、函数和字符串。</div>
-      //         <div>当传入函数时，函数参数为field，返回数组格式的过滤条件即可。</div>
-      //         <div>当传入字符串时，要求字符串格式为:function(field){return [[\"name\", \"contains\", \"三\"]]}。</div>
-      //         <div>设计器中右侧面板显示为多行文本。</div>
-      //         `,
-      //     },
-      //     {
-      //       "label": "数组",
-      //       "type": "steedos-select-user",
-      //       "name": "admins",
-      //       "multiple": true,
-      //       // "filters": [["name", "contains", "王"]],
-      //     },
-      //     {
-      //       "label": "函数",
-      //       "type": "steedos-select-user",
-      //       "name": "admins",
-      //       "multiple": true,
-      //       "filters": function(field: any){
-      //         return [["name", "contains", "王"]];
-      //       },
-      //     },
-      //     {
-      //       "label": "字符串",
-      //       "type": "steedos-select-user",
-      //       "name": "admins",
-      //       "multiple": true,
-      //       "filters": `function(field){
-      //         return [["name", "contains", "王"]];
-      //       }`,
-      //     }
-      //   ]
-      // }
     ]
     }}
     assetUrls={assetUrls}
