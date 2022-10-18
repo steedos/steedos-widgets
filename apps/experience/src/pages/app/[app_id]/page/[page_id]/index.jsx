359322a508f949a42812cd539ff8d1535d1679a3
baozhoutao <baozhoutao@hotoa.com>
2022-10-18 14:02:07 +0800

    新版nextjs web端 tab切换的时候刷新有重复刷新的问题
    https://gitlab.steedos.cn/medsci/ms-scrm/-/issues/39
    https://gitlab.steedos.cn/medsci/ms-scrm/-/issues/20

--- apps/experience/src/pages/app/[app_id]/page/[page_id]/index.jsx
+++ apps/experience/src/pages/app/[app_id]/page/[page_id]/index.jsx
@@ -2,28 +2,19 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-07-04 11:24:28
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-09-06 09:44:26
+ * @LastEditTime: 2022-10-18 14:01:14
  * @Description:
  */
 import dynamic from "next/dynamic";
-import Document, { Script, Head, Main, NextScript } from "next/document";
 import React, { useState, useEffect, Fragment } from "react";
 import { useRouter } from "next/router";
 import { getPage } from "@steedos-widgets/amis-lib";
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { PageRender } from "@/components/PageRender";
-import { setRootUrl } from "@steedos-widgets/amis-lib";
 
-export default function Page({ publicEnv }) {
+export default function Page(props) {
   const router = useRouter();
   const { app_id, page_id } = router.query;
   const [page, setPage] = useState(null);
-
-  if (typeof window !== "undefined") {
-    Builder.set({ env: publicEnv });
-    setRootUrl(publicEnv.STEEDOS_ROOT_URL);
-  }
 
   useEffect(() => {
     if (!page_id) return;
@@ -31,10 +22,9 @@ export default function Page({ publicEnv }) {
       setPage(data);
     });
   }, [app_id, page_id]);
-
   return (
     <>
-      {page && page.schema && (
+      {page && page.schema && page.name === page_id && (
         <PageRender
           id="amis-root"
           className="overflow-auto"
@@ -45,28 +35,4 @@ export default function Page({ publicEnv }) {
       )}
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  const session =
-    context.req.session ||
-    (await unstable_getServerSession(context.req, context.res, authOptions));
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login?callbackUrl=/app",
-        permanent: false,
-      },
-    };
-  }
-
-  // 临时方案, 还未找到合适的方式来处理前端默认配置
-  return {
-    props: {
-      publicEnv: {
-        STEEDOS_ROOT_URL: process.env.STEEDOS_ROOT_URL,
-        STEEDOS_EXPERIENCE_ASSETURLS: process.env.STEEDOS_EXPERIENCE_ASSETURLS,
-      },
-    },
-  };
 }
