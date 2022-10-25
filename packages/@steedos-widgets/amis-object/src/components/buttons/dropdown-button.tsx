b37c62ffb5f3b2632e264769139797c2b2d092ad
baozhoutao <baozhoutao@hotoa.com>
2022-10-25 16:38:05 +0800

    优化下拉按钮样式
    https://github.com/steedos/steedos-webapp-nextjs/issues/55

--- packages/@steedos-widgets/amis-object/src/components/buttons/dropdown-button.tsx
+++ packages/@steedos-widgets/amis-object/src/components/buttons/dropdown-button.tsx
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-10-22 17:26:21
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-10-25 11:20:35
+ * @LastEditTime: 2022-10-25 16:21:07
  * @Description: 
  */
 import React, { useEffect, useState } from 'react'
@@ -14,7 +14,7 @@ import { createObject } from '@steedos-widgets/amis-lib';
 const getMenu = (render, buttons, props)=>{
     return <Menu
     items={map(buttons, (button)=>{
-        button.className = button.className ? `${button.className} steedos-dropdown-button-item py-1` : 'steedos-dropdown-button-item py-1'
+        button.className = button.className ? `${button.className} steedos-dropdown-button-item py-1 text-left w-full` : 'steedos-dropdown-button-item py-1 text-left w-full'
         return {
             key: button.name,
             className: 'py-0',
