525fc93251c6ec756d6da2900e7976c88a0a477e
baozhoutao <baozhoutao@hotoa.com>
2022-11-08 14:04:24 +0800

    add jquery

--- apps/experience/src/pages/_document.js
+++ apps/experience/src/pages/_document.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-08-02 20:17:43
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-08-12 17:40:55
+ * @LastEditTime: 2022-11-08 11:28:43
  * @Description: 
  */
 import Document, { Html, Head, Main, Script, NextScript } from 'next/document'
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
           <script src="https://unpkg.steedos.cn/amis@2.3.0/sdk/sdk.js"></script>
           <script src="https://unpkg.steedos.cn/lodash/lodash.min.js"></script>
           <script src="https://unpkg.steedos.cn/moment/min/moment.min.js"></script>
+          <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
           <link rel="stylesheet" href="https://unpkg.steedos.cn/@salesforce-ux/design-system@2.19.0/assets/styles/salesforce-lightning-design-system.min.css" />
           <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.3.0/lib/themes/antd.css" />
           <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.3.0/lib/helper.css" />
