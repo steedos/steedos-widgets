6f84e0fcd5685d55985f138c90e7f220c1ab7ccd
baozhoutao <baozhoutao@hotoa.com>
2022-11-14 10:15:52 +0800

    调整loading效果

--- packages/@steedos-widgets/amis-object/src/components/buttons/button.tsx
+++ packages/@steedos-widgets/amis-object/src/components/buttons/button.tsx
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-10-21 10:27:43
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-11-12 11:15:42
+ * @LastEditTime: 2022-11-14 10:15:41
  * @Description: 
  */
 import React, { useEffect, useState } from 'react'
@@ -53,7 +53,10 @@ export const AmisObjectButton = (props) => {
         });
     };
     if(!button){
-        return (<>loading...</>)
+        return (<><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="animate-spin w-4 h-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+      </svg>
+      </>)
     }
     if ((button as any).type === "amis_button") {
         const amisSchema = (button as any).amis_schema;
