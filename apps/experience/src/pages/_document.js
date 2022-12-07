a4dd372542c83964f3c63ff15305af00bf9dfd04
baozhoutao <baozhoutao@hotoa.com>
2022-12-07 13:34:00 +0800

    amis 2.4.0

--- apps/experience/src/pages/_document.js
+++ apps/experience/src/pages/_document.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-08-02 20:17:43
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-11-08 11:28:43
+ * @LastEditTime: 2022-12-07 13:33:48
  * @Description: 
  */
 import Document, { Html, Head, Main, Script, NextScript } from 'next/document'
@@ -21,14 +21,14 @@ export default class MyDocument extends Document {
           <meta name="application-name" content="Steedos" />
   
           <meta name="theme-color" content="#ffffff" />
-          <script src="https://unpkg.steedos.cn/amis@2.3.0/sdk/sdk.js"></script>
+          <script src="https://unpkg.steedos.cn/amis@2.4.0/sdk/sdk.js"></script>
           <script src="https://unpkg.steedos.cn/lodash/lodash.min.js"></script>
           <script src="https://unpkg.steedos.cn/moment/min/moment.min.js"></script>
           <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
           <link rel="stylesheet" href="https://unpkg.steedos.cn/@salesforce-ux/design-system@2.19.0/assets/styles/salesforce-lightning-design-system.min.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.3.0/lib/themes/antd.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.3.0/lib/helper.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.3.0/sdk/iconfont.css" />
+          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.4.0/lib/themes/antd.css" />
+          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.4.0/lib/helper.css" />
+          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.4.0/sdk/iconfont.css" />
           <link rel="stylesheet" href="https://unpkg.steedos.cn/@fortawesome/fontawesome-free@6.2.0/css/all.min.css" />
 
         </Head>
