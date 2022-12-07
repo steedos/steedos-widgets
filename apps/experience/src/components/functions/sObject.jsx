5b04d8eb1ac614bc8db565b8ef73b0c2d2f32b88
baozhoutao <baozhoutao@hotoa.com>
2022-12-07 18:04:29 +0800

    标准化批量删除按钮; 删除无用代码

--- apps/experience/src/components/functions/sObject.jsx
+++ apps/experience/src/components/functions/sObject.jsx
@@ -1,79 +1,12 @@
-import { Form } from '@/components/object/Form'
-import { Button, Space } from 'antd';
-import { isFunction, each } from 'lodash';
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-08-31 15:04:42
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-11-29 18:43:12
+ * @Description: 
+ */
+import { each } from 'lodash';
 import { fetchAPI, getUISchema } from '@steedos-widgets/amis-lib';
-
-const editRecordHandle = (props)=>{
-    const { appId, name, title, objectName, recordId, type, options, router, refId, data, onSubmitted, onCancel, formFactor } = props;
-    if(type === 'modal'){
-        SteedosUI.Modal(Object.assign({
-            name: name,
-            title: title,
-            destroyOnClose: true,
-            maskClosable: false,
-            keyboard: false, // 禁止 esc 关闭
-            // footer: null,
-            cancelText: '取消',
-            okText: '确定',
-            onOk: (e)=>{
-                const scope = SteedosUI.getRef(SteedosUI.getRefId({type: `form`, appId: appId, name: objectName}));
-                const form = scope.getComponentByName(`page_edit_${recordId}.form_edit_${recordId}`);
-                form.handleAction({}, { type: "submit" }).then((data)=>{
-                    if(data){
-                        SteedosUI.getRef(name).close();
-                        if(isFunction(onSubmitted)){
-                            onSubmitted(e, data)
-                        }
-                    }
-                })
-            },
-            onCancel: (e)=>{
-                SteedosUI.getRef(name).close();
-                if(isFunction(onCancel)){
-                    onCancel(e)
-                }
-            },
-            bodyStyle: {padding: "0px", paddingTop: "12px"},
-            children: <Form appId={appId} objectName={objectName} recordId={recordId} data={data} formFactor={formFactor}></Form>
-        }, options?.props));
-    }else if(type === 'drawer'){
-        SteedosUI.Drawer(Object.assign({
-            name: name,
-            title: title,
-            destroyOnClose: true,
-            maskClosable: false,
-            footer: null,
-            bodyStyle: {padding: "0px", paddingTop: "12px"},
-            children: <Form appId={appId} objectName={objectName} recordId={recordId} data={data} formFactor={formFactor}></Form>,
-            mask: false,
-            size: 'large',
-            style: null,
-            extra: (
-            <Space>
-                <Button onClick={(e)=>{
-                    SteedosUI.getRef(name).close();
-                    if(isFunction(onCancel)){
-                        onCancel(e)
-                    }
-                }}>取消</Button>
-                <Button type='primary' onClick={(e)=>{
-                    const scope = SteedosUI.getRef(SteedosUI.getRefId({type: `form`, appId: appId, name: objectName}));
-                    const form = scope.getComponentByName(`page_edit_${recordId}.form_edit_${recordId}`);
-                    form.handleAction({}, { type: "submit" }).then((data)=>{
-                        if(data){
-                            SteedosUI.getRef(name).close();
-                            if(isFunction(onSubmitted)){
-                                onSubmitted(e, data)
-                            }
-                        }
-                    })
-                }}>确认</Button>
-            </Space>)
-        }, options?.props))
-    }else{
-        router.push(`/app/${appId}/${objectName}/view/new`)
-    }
-}
 
 const getGraphqlFieldsQuery = (fields)=>{
     const fieldsName = ['_id'];
@@ -104,13 +37,6 @@ const getFindOneQuery = (objectName, id, fields)=>{
 }
 
 export const SObject = {
-    //TODO 清理router参数传递
-    newRecord: (props)=>{
-        return editRecordHandle(props)
-    },
-    editRecord: (props)=>{
-        return editRecordHandle(props)
-    },
     getRecord: async (objectName, recordId, fields)=>{
         const result = await fetchAPI('/graphql', {
             method: 'post',
