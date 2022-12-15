27a27aa9cfa24ccc6ba3f3a0922f7631ddc24782
baozhoutao <baozhoutao@hotoa.com>
2022-12-15 19:05:56 +0800

    处理通知数据显示

--- packages/@steedos-widgets/amis-object/src/amis/AmisGlobalHeader.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisGlobalHeader.tsx
@@ -1,13 +1,9 @@
 
-
-
-
-
   /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-01 14:44:57
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-12-15 16:18:07
+ * @LastEditTime: 2022-12-15 19:03:19
  * @Description: 
  */
 
@@ -99,18 +95,86 @@ export const AmisGlobalHeader = async (props) => {
               }
             ],
             "overlay": [
-              {
-                "type": "steedos-object-table",
-                "label": "对象表格",
-                "objectApiName": "notifications",
-                "className": "bg-white",
-                "columns": [
-                  {
-                    "field": "name"
+                {
+                    "type": "service",
+                    "body": [
+                        {
+                            "type": "wrapper",
+                            "body": [
+                              {
+                                "type": "tpl",
+                                "tpl": "<p>通知</p>",
+                                "inline": false,
+                                "id": "u:0b4404270d4b"
+                              }
+                            ],
+                            "className": "border-0 border-b text-base\t",
+                            "id": "u:ad394b5f49f6",
+                            "size": "sm"
+                          },
+                        {
+                            "type": "each",
+                            className: "overflow-auto max-h-96",
+                            "name": "notifications",
+                            "items": {
+                              "type": "tpl",
+                              "tpl": "<div class='flex items-center p-4 hover:bg-sky-50'><img src='<%=data.context.rootUrl + `/avatar/` + data.from%>' alt='' class='h-10 w-10 flex-none rounded-full'><div class='ml-4 flex-auto'><div class='font-medium'><%=data.name%></div><div class='mt-1 text-slate-700'><%=data.body%></div><div class='mt-1 text-slate-700'><%=moment(data.created).fromNow()%><abbr class='slds-text-link slds-m-horizontal_xxx-small <%=data.is_read ? 'hidden' : ''%>' title='unread'>●</abbr></div></div></div>",
+                              "id": "u:07ece657c7b7"
+                            },
+                            "id": "u:18da41dab9ca"
+                        },
+                        {
+                            "type": "button",
+                            "label": "全部标记为已读",
+                            "id": "u:5530f3779e3a",
+                            "block": true,
+                            "size": "md",
+                            "onEvent": {
+                              "click": {
+                                "actions": [
+                                  {
+                                    "componentId": "",
+                                    "args": {
+                                      "api": {
+                                        "url": "${context.rootUrl}/api/v4/notifications/all/markReadAll",
+                                        "method": "post",
+                                        "headers": {
+                                          "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+                                        }
+                                      },
+                                      "messages": {
+                                        "success": "已全部标记为已读"
+                                      }
+                                    },
+                                    "actionType": "ajax"
+                                  }
+                                ],
+                                "weight": 0
+                              }
+                            }
+                          }
+                    ],
+                    "id": "u:aba521eed5b7",
+                    "messages": {
+                    },
+                    "api": {
+                      "method": "post",
+                      "url": "${context.rootUrl}/graphql",
+                      "data": {
+                        "&": "$$",
+                        "context": "${context}",
+                        "userId": "${context.userId}"
+                      },
+                      "dataType": "json",
+                      "requestAdaptor": "const { userId } = api.data;\napi.data = {\n    query: `{\n        notifications(filters: [\"owner\",\"=\",\"${userId}\"], sort: \"created desc,name\", top : 10){\n          _id,name,body,related_to,related_name,url,owner,is_read,from,created\n        },\n        unReadCount: notifications__count(filters: [[\"owner\",\"=\",\"${userId}\"], [\"is_read\", \"!=\", true]])\n    }`\n}",
+                      "headers": {
+                        "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+                      },
+                      "adaptor": "console.log(payload); return payload.data"
+                    },
+                    "interval": 30000,
+                    "silentPolling": true
                   }
-                ],
-                "id": "u:3585d3f4958a"
-              }
             ],
             "id": "u:857e8161c96b",
             "className": "antd-Action",
