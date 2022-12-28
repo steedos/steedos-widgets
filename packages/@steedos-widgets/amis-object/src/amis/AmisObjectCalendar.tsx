79e8a5ca7e5a79a74d45268e8c7a6de49bd0b3fc
殷亮辉 <yinlianghui@hotoa.com>
2022-12-28 06:44:12 +0000

    #186 日历视图组件新建编辑记录时异常

--- packages/@steedos-widgets/amis-object/src/amis/AmisObjectCalendar.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisObjectCalendar.tsx
@@ -8,13 +8,12 @@
 import { getCalendarSchema } from '@steedos-widgets/amis-lib'
 
 export const AmisObjectCalendar = async (props) => {
-  console.log(`AmisObjectCalendar props`, props)
+  // console.log(`AmisObjectCalendar props`, props)
   const { $schema, top, sort, filter, title, currentView, startDateExpr, endDateExpr, allDayExpr, textExpr, data, defaultData, className=""} = props;
 
   let objectApiName = props.objectApiName || "events";
 
   const amisSchemaData = Object.assign({}, data, defaultData);
-  // const listViewId = ctx?.listViewId || amisSchemaData.listViewId;
   let schema: any = (await getCalendarSchema(amisSchemaData.appId, objectApiName, {
     title,
     currentView,
@@ -22,8 +21,14 @@ export const AmisObjectCalendar = async (props) => {
     endDateExpr,
     allDayExpr,
     textExpr
-  }, { top, sort, filter, className }));
+  }, { top, sort, filter }));
+  const uiSchema = schema.uiSchema;
   const amisSchema = schema.amisSchema;
-  amisSchema.data = Object.assign({}, amisSchema.data, amisSchemaData);
-  return amisSchema;
+  const serviceData = Object.assign({}, amisSchema.data, amisSchemaData, { objectName: objectApiName, uiSchema });
+  return {
+    "type": "service",
+    "body": amisSchema,
+    "className": `${className}`,
+    "data": serviceData
+  }
 }
