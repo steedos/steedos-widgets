3b7729be738f66787b03904988ad6bc831eff0bf
baozhoutao <baozhoutao@hotoa.com>
2023-01-04 09:58:07 +0800

    fix: 行级按钮样式

--- packages/@steedos-widgets/amis-object/src/index.ts
+++ packages/@steedos-widgets/amis-object/src/index.ts
@@ -3,6 +3,7 @@ import './styles/antd.css';
 import './styles/tailwind.css';
 import './styles/object.less';
 import './styles/dropdown.css';
+import './styles/dropdown-button.less'
 
 export * from './components';
 export * from './amis';
