2081b424a8c23ff89e7d04097a76fddaa0d65041
廖大雪 <2291335922@qq.com>
2023-01-05 16:29:02 +0800

    fix bug：设计器中单独选中标题面板组件不显示。解决办法，onEvent添加默认值
    https://github.com/steedos/steedos-webapp-nextjs/issues/199

--- packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetailHeader.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetailHeader.tsx
@@ -5,10 +5,32 @@
  * @LastEditTime: 2022-12-02 18:05:51
  * @Description: 
  */
-import { getRecordDetailHeaderSchema } from '@steedos-widgets/amis-lib'
+import { getRecordDetailHeaderSchema , getUISchema} from '@steedos-widgets/amis-lib'
 
 export const AmisRecordDetailHeader = async (props) => {
-  const { $schema, recordId, onEvent } = props;
+  const objectUiSchema = await getUISchema(props.objectApiName || "space_users", false);
+  const defaultOnEvent = {
+    "recordLoaded": {
+      "actions": [
+        {
+          "actionType": "reload",
+          "data": {
+            "name": `\${record.${objectUiSchema?.NAME_FIELD_KEY || 'name'}}`
+          }
+        },
+        {
+          "actionType": "setValue",
+          "args": {
+            value: {
+              "record": "${record}",
+              "recordLoaded": true,
+            }
+          }
+        }
+      ]
+    }
+  }
+  const { $schema, recordId, onEvent = defaultOnEvent } = props;
   let objectApiName = props.objectApiName || "space_users";
   const schema = (await getRecordDetailHeaderSchema(objectApiName, recordId)).amisSchema;
   return Object.assign({}, schema, {onEvent: onEvent})
