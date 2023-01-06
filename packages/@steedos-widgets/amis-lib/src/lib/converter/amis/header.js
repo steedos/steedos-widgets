76df28bf3e6fcf2f5b197604e812e3155fb54b4b
庄建国 <zhuangjianguo@steedos.com>
2023-01-06 06:01:39 +0000

    theme

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/header.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/header.js
@@ -269,23 +269,22 @@ export async function getObjectListHeader(objectSchema, listViewName, ctx) {
     ctx = {};
   }
   let firstLineSchema = getObjectListHeaderFirstLine(objectSchema, listViewName, ctx);
-  let secordLineSchema = await getObjectListHeaderSecordLine(objectSchema, listViewName, ctx);
+  // let secordLineSchema = await getObjectListHeaderSecordLine(objectSchema, listViewName, ctx);
   // let body = [firstLineSchema, secordLineSchema];
   let body = [firstLineSchema];
-  let roundedCss = "sm:rounded-tl sm:rounded-tr";
-  if (ctx.onlyFirstLine) {
-    body = [firstLineSchema];
-  }
-  else if (ctx.onlySecordLine) {
-    // 列表视图自定义amisSchema时不能加圆角
-    roundedCss = "";
-    // body = [secordLineSchema];
-  }
-  // className中不可以加p-4，否则它会优先于后面的pt-0，边距效果依赖wrapper自带的antd-Wrapper--md样式类
+  // let roundedCss = "";
+  // if (ctx.onlyFirstLine) {
+  //   body = [firstLineSchema];
+  // }
+  // else if (ctx.onlySecordLine) {
+  //   // 列表视图自定义amisSchema时不能加圆角
+  //   roundedCss = "";
+  //   // body = [secordLineSchema];
+  // }
   let headerSchema = [{
     "type": "wrapper",
     "body": body,
-    "className": `bg-gray-100 ${roundedCss} ${ctx.onlySecordLine && "pt-0"}`
+    "className": `bg-gray-100 sm:rounded-tl sm:rounded-tr p-4`
   }];
   // const fieldsFilterBarSchema = await getObjectListHeaderFieldsFilterBar(objectSchema, listViewName, ctx);
   // headerSchema.push(fieldsFilterBarSchema);
