28891abebcfc5436c8898514a98a6e998c4f7c63
廖大雪 <2291335922@qq.com>
2023-02-07 18:02:52 +0800

    getObjectRelatedList 函数迁移、优化
    https://github.com/steedos/steedos-platform/issues/4226

--- packages/@steedos-widgets/amis-lib/src/lib/objectsRelated.js
+++ packages/@steedos-widgets/amis-lib/src/lib/objectsRelated.js
@@ -30,7 +30,7 @@ export const getRelatedFieldValue = (masterObjectName, record_id, uiSchema, fore
 }
 
 // 获取所有相关表
-export async function getAmisObjectRelatedList(
+export async function getObjectRelatedList(
     appName,
     objectName,
     recordId,
@@ -42,117 +42,28 @@ export async function getAmisObjectRelatedList(
     if(!isEmpty(relatedLists)){
         for (const relatedList of relatedLists) {
             const arr = relatedList.related_field_fullname.split(".");
-            let filter = null;
-            const refField = await getField(arr[0], arr[1]);
-            if(refField){
-                if (
-                    refField._reference_to ||
-                    (refField.reference_to && !isString(refField.reference_to))
-                ) {
-                    filter = [
-                        [`${arr[1]}/o`, "=", objectName],
-                        [`${arr[1]}/ids`, "=", recordId],
-                    ];
-                } else {
-                    filter = [`${arr[1]}`, "=", recordId];
-                }
-                const relatedUiSchema = await getUISchema(arr[0]);
-                if(relatedUiSchema){
-                    let fields = [];
-                    forEach(relatedList.field_names , (fName)=>{
-                        const field = find(relatedUiSchema.fields, (item)=>{
-                            return item.name === fName
-                        })
-                        if(field){
-                            fields.push(field);
-                        }
-                    })
-                    // TODO: 也需在amisSchema上再添加一层service
-                    related.push({
-                        masterObjectName: objectName,
-                        object_name: arr[0],
-                        foreign_key: arr[1],
-                        schema: {
-                            uiSchema: relatedUiSchema,
-                            amisSchema: await getObjectList(relatedUiSchema, fields, {
-                                tabId: objectName,
-                                appId: appName,
-                                objectName: arr[0],
-                                formFactor: formFactor,
-                                globalFilter: filter,
-                                buttons: await getListViewItemButtons(relatedUiSchema, {isMobile: false})
-                            })
-                        }
-                    });
-                }
-            }
+            related.push({
+                masterObjectName: objectName,
+                object_name: arr[0],
+                foreign_key: arr[1],
+                label: relatedList.label,
+                columns: relatedList.field_names,
+                sort: relatedList.sort,
+                filters: relatedList.filters,
+                visible_on: relatedList.visible_on,
+                page_size: relatedList.page_size
+            }); 
         }
-
     }else{
         const details = [].concat(uiSchema.details || []);
-
         for (const detail of details) {
             const arr = detail.split(".");
-            const relatedUiSchema = await getUISchema(arr[0]);
-            if(relatedUiSchema){
-                let filter = null;
-                const refField = await getField(arr[0], arr[1]);
-                if (
-                    refField._reference_to ||
-                    (refField.reference_to && !isString(refField.reference_to))
-                ) {
-                    filter = [
-                        [`${arr[1]}/o`, "=", objectName],
-                        [`${arr[1]}/ids`, "=", recordId],
-                    ];
-                } else {
-                    filter = [`${arr[1]}`, "=", recordId];
-                }
-                const relatedSchema =  await getListSchema(appName, arr[0], "all", {
-                    // globalFilter: filter,
-                    formFactor: formFactor,
-                    buttons: await getListViewItemButtons(relatedUiSchema, {isMobile: false})
-                });
-
-                const relatedAmisSchema = relatedSchema.amisSchema;
-                const recordRelatedListHeader = await getObjectRecordDetailRelatedListHeader(relatedUiSchema);
-                relatedAmisSchema.headerSchema = recordRelatedListHeader;
-                // relatedAmisSchema.listSchema =  { headerToolbar:[],columnsTogglable:false };
-                relatedAmisSchema.headerToolbar =  [];
-                relatedAmisSchema.columnsTogglable =  false ;
-                delete relatedAmisSchema.ctx;
-                relatedSchema.amisSchema = {
-                    type: "service",
-                    className: "b-a m-b",
-                    data: {
-                        masterObjectName: objectName,
-                        masterRecordId: "${recordId}",
-                        relatedKey: arr[1],   
-                        objectName: arr[0],
-                        listViewId: `amis-\${appId}-${arr[0]}-listview`,
-                    },
-                    body:[
-                        {
-                            ...relatedAmisSchema,
-                            data: {
-                                filter: ["${relatedKey}", "=", "${masterRecordId}"],
-                                objectName: "${objectName}",
-                                recordId: "${masterRecordId}",
-                                defaultData: {
-                                    ...{[arr[1]]: getRelatedFieldValue(objectName, recordId, relatedSchema.uiSchema, arr[1])}
-                                }
-                            }
-                        }
-                    ]
-                }
-
-                related.push({
-                    masterObjectName: objectName,
-                    object_name: arr[0],
-                    foreign_key: arr[1],
-                    schema: relatedSchema,
-                });
-            }
+            related.push({
+                masterObjectName: objectName,
+                object_name: arr[0],
+                foreign_key: arr[1],
+                label: uiSchema.label
+            });
         }
     }
 
