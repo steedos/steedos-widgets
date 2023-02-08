b4eedf26881f66be3a92ad1c55cb3a971f21d843
庄建国 <zhuangjianguo@steedos.com>
2023-02-08 02:06:03 +0000

    accounts@2.4.0-beta.19

--- packages/@steedos-widgets/amis-object/src/index.ts
+++ packages/@steedos-widgets/amis-object/src/index.ts
@@ -4,6 +4,7 @@ import './styles/tailwind.css';
 import './styles/object.less';
 import './styles/dropdown.css';
 import './styles/dropdown-button.less';
+import './styles/steedos.css';
 
 import './styles/data-table.less';
 
