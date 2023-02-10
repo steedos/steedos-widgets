5f40ba385e0835cc0ab8567b7f102f9004bdd7c5
庄建国 <zhuangjianguo@steedos.com>
2023-02-10 10:03:57 +0000

    优化列表页样式

--- apps/experience/src/pages/app/[app_id]/[tab_id]/grid/[listview_id].jsx
+++ apps/experience/src/pages/app/[app_id]/[tab_id]/grid/[listview_id].jsx
@@ -61,7 +61,7 @@ export default function Page ({ formFactor: defaultFormFactor }) {
     "showDisplayAs": (defaultFormFactor !== 'SMALL'),
     "formFactor": formFactor,
     "className": displayAs === 'grid'? 
-      "absolute inset-0 sm:m-3 mb-0 sm:border sm:shadow sm:rounded border-slate-300 border-solid bg-gray-100" : 
+      "absolute inset-0 sm:m-3 sm:mb-0 sm:border sm:shadow sm:rounded border-slate-300 border-solid bg-gray-100" : 
       "absolute top-0 bottom-0 w-[388px] border-r border-slate-300 border-solid bg-gray-100"
   }
 
