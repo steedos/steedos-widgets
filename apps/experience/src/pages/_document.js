0984914e4d6f22aee0a0cbd6e7ac44cf6befe033
庄建国 <zhuangjianguo@steedos.com>
2023-02-13 06:40:26 +0000

    统一加载全局资产包

--- apps/experience/src/pages/_document.js
+++ apps/experience/src/pages/_document.js
@@ -27,22 +27,23 @@ export default class MyDocument extends Document {
           <script src="https://unpkg.steedos.cn/i18next@22.4.8/dist/umd/i18next.min.js"></script>
           <script>
             window.STEEDOS_ROOT_URL = `{process.env.STEEDOS_ROOT_URL}`;
+            window.STEEDOS_EXPERIENCE_ASSETURLS = `{process.env.STEEDOS_EXPERIENCE_ASSETURLS}`;
           </script>
           <script src="/steedos-init.js"></script>
           <script src={`${process.env.STEEDOS_ROOT_URL}/steedos_dynamic_scripts.js`} ></script>
 
           <link rel="stylesheet" href="/tailwind-base.css" />
           <link rel="stylesheet" href="https://unpkg.steedos.cn/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.5.2/lib/themes/antd.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.5.2/lib/helper.css" />
-          <link rel="stylesheet" href="https://unpkg.steedos.cn/amis@2.5.2/sdk/iconfont.css" />
+          <link rel="stylesheet" href={`https://unpkg.steedos.cn/amis@${process.env.STEEDOS_EXPERIENCE_AMIS_VERSION}/lib/themes/antd.css`} />
+          <link rel="stylesheet" href={`https://unpkg.steedos.cn/amis@${process.env.STEEDOS_EXPERIENCE_AMIS_VERSION}/lib/helper.css`} />
+          <link rel="stylesheet" href={`https://unpkg.steedos.cn/amis@${process.env.STEEDOS_EXPERIENCE_AMIS_VERSION}/sdk/iconfont.css`} />
           <link rel="stylesheet" href="https://unpkg.steedos.cn/@fortawesome/fontawesome-free@6.2.0/css/all.min.css" />
 
-          {process.env.NODE_ENV !== 'production' && (
+          {/* {process.env.NODE_ENV !== 'production' && (
             <>
               <link rel="stylesheet" href="https://cdn.tailwindcss.com" />
             </>
-          )}
+          )} */}
 
         </Head>
         <body className=''>
