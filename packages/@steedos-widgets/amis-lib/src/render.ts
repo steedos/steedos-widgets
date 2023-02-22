1f704b29acebd3e2b20d6c3deb36d756ef22131a
baozhoutao <baozhoutao@hotoa.com>
2023-02-22 18:20:03 +0800

    fix bug

--- packages/@steedos-widgets/amis-lib/src/render.ts
+++ packages/@steedos-widgets/amis-lib/src/render.ts
@@ -4,7 +4,7 @@ import { getRootUrl, getSteedosAuth } from "./lib/steedos.client";
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-12-17 17:03:40
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-12-17 17:03:42
+ * @LastEditTime: 2023-02-22 18:19:44
  * @Description: 
  */
 export const getDefaultRenderData = ()=>{
@@ -13,13 +13,13 @@ export const getDefaultRenderData = ()=>{
       context: {
           rootUrl: getRootUrl(),
           userId: steedosAuth.userId,
-          tenantId: steedosAuth.spaceId,
+          tenantId: steedosAuth.tenantId,
           authToken: steedosAuth.authToken,
           user: steedosAuth
       },
       global: {
           userId: steedosAuth.userId,
-          spaceId: steedosAuth.spaceId,
+          spaceId: steedosAuth.tenantId,
           user: steedosAuth, 
           now: new Date(),
           // mode: mode //由表单提供
