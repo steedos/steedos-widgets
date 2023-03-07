ca7b631001b9b50a1b30a514797fd73bd3c9a930
廖大雪 <2291335922@qq.com>
2023-03-07 11:09:48 +0800

    换一种方案解决，会影响depend_on的功能
    https://github.com/steedos/steedos-platform/issues/4205

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/lookup.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/lookup.js
@@ -346,9 +346,9 @@ export async function lookupToAmisSelect(field, readonly, ctx){
             const data = payload.data;
             var defaultValueOptions = data.defaultValueOptions;
             // 字段值下拉选项合并到options中
-            data.options = _.unionWith(defaultValueOptions, data.options, function(a,b){
-                return a["value"]=== b["value"];
-            });
+            // data.options = _.unionWith(defaultValueOptions, data.options, function(a,b){
+            //     return a["value"]=== b["value"];
+            // });
             delete data.defaultValueOptions;
             payload.data.options = data.options;
             return payload;
