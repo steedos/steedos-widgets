9563bb6f44ca7fcd5f48f655f2717745be3c6b93
殷亮辉 <yinlianghui@hotoa.com>
2023-03-14 15:09:32 +0800

    Revert "amis选人控件需支持指定人员可选择范围"
    This reverts commit cec8e9f63f6653a8482dc844469657f34a0f9256.

--- packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
@@ -1,23 +1,15 @@
-/*
- * @Author: 殷亮辉 yinlianghui@hotoa.com
- * @Date: 2023-02-07 16:41:31
- * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2023-03-13 19:40:12
- * @FilePath: /steedos-ee/Users/yinlianghui/Documents/GitHub/steedos-webapp-nextjs/packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { getSelectUserSchema } from '@steedos-widgets/amis-lib';
 
 export const AmisSelectUser = async (props) => {
   console.log(`AmisSelectUser props`, props)
-  const { $schema, data, options } = props;
+  const { $schema, data } = props;
   const amisSchema: any = await getSelectUserSchema({
     name: $schema.name,
     multiple: $schema.multiple,
     searchable: $schema.searchable,
     onEvent: $schema.onEvent,
     filters: $schema.filters
-  }, false, { options });
+  }, false, {});
   amisSchema.name = $schema.name;
   amisSchema.label = $schema.label;
   return amisSchema;
