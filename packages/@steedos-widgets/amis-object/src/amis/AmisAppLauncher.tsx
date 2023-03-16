8407ec1704b68e6e3c25c0bd923e3c9521364e93
涂佳俊 <tujiajun@steedos.com>
2023-03-16 10:44:00 +0000

    前端订阅 steedos_keyvalues 的数据，有变化时broadcast amis event @data.changed.steedos_keyvalues #4517

--- packages/@steedos-widgets/amis-object/src/amis/AmisAppLauncher.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisAppLauncher.tsx
@@ -1,159 +1,274 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-01 14:44:57
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-12-16 11:18:39
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2023-03-13 16:19:22
  * @Description: 
  */
 
 export const AmisAppLauncher = async (props) => {
-    let { app, data, className, showAppName = true, appNameClassName = '' } = props;
-    if(!app){
-      app = data.context.app ;
-    }
-    
-    const formFactor = window.innerWidth < 768 ? 'SMALL' : 'LARGE';
-    const mobile = formFactor === "SMALL" ? true : false;
+  let { app, data, className, showAppName = true, appNameClassName = '' } = props;
+  if (!app) {
+    app = data.context.app;
+  }
 
-    return {
-        type: 'service',
-        className,
-        body: [
-            {
-                "type": "button",
-                "actionType": "dialog",
-                "className": "flex items-center",
-                "body": [
-                  {
-                    "type": "tpl",
-                    "className": "flex items-center",
-                    "tpl": "<div aria-haspopup='true' title='Open App Launcher' class='slds-icon-waffle_container slds-context-bar__button' title='Open App Launcher' type='button'><span class='slds-icon-waffle'><span class='slds-r1'></span><span class='slds-r2'></span><span class='slds-r3'></span><span class='slds-r4'></span><span class='slds-r5'></span><span class='slds-r6'></span><span class='slds-r7'></span><span class='slds-r8'></span><span class='slds-r9'></span></span></div>",
-                  },
-                  {
-                      type: 'tpl',
-                      className: `text-xl ml-4 mr-4 text-black nowrap ${appNameClassName} `,
-                      tpl: '${app.name}',
-                      hiddenOn: `${!!app || !!!showAppName}`
-                  }
-                ],
-                "dialog": {
-                  "size": "xl",
-                  "title": {
-                    "type": "tpl",
-                    "tpl": "应用程序启动器",
-                    "className": "block text-xl text-center"
-                  },
-                  "actions": [
-                  ],
-                  "body": [
-                    {
-                      "type": "service",
-                      "id": "u:0f6224a0836f",
-                      "affixFooter": false,
-                      "body": [
-                        {
-                          "type": "collapse-group",
-                          "activeKey": [
-                            "1",
-                            "2"
-                          ],
-                          "body": [
-                            {
-                              "type": "collapse",
-                              "key": "1",
-                              "header": "所有应用",
-                              "body": [
-                                {
-                                  "type": "each",
-                                  "name": "app_items",
-                                  "items": {
-                                    "type": "button",
-                                    "level": "link",
-                                    "actionType": "link",
-                                    "link": "${path}",
-                                    "body": [{
-                                      "type": "tpl",
-                                      "tpl": "<div class='slds-app-launcher__tile slds-text-link_reset'><div class='slds-app-launcher__tile-figure'><svg class='w-12 h-12 slds-icon slds-icon_container slds-icon-standard-${REPLACE(icon, '_', '-')}' aria-hidden='true'><use xlink:href='/assets/icons/standard-sprite/svg/symbols.svg#${icon}'></use></svg><span class='slds-assistive-text'>${name}</span></div><div class='slds-app-launcher__tile-body'><span class='slds-link text-blue-600 text-lg'><span title='${name}'>${name}</span></span><div style='display: -webkit-box; -webkit-line-clamp: 1;-webkit-box-orient: vertical;overflow: hidden;'><span title='${description}'>${description}</span></div></div></div>",
-                                    }],
-                                    "onEvent": {
-                                      "click": {
-                                        "actions": [
-                                          {
-                                            "actionType": "closeDialog"
-                                          }
-                                        ]
-                                      }
-                                    },
-                                    "inline": true,
-                                    "style": {
-                                    },
-                                    "className": "slds-p-horizontal_small slds-size_1-of-1 slds-medium-size_1-of-3"
-                                  },
-                                  "className": "slds-grid slds-wrap slds-grid_pull-padded"
-                                }
-                              ]
-                            },
-                            {
-                              "type": "collapse",
-                              "key": "2",
-                              "header": "所有项目",
-                              "body": [
-                                {
-                                  "type": "each",
-                                  "name": "object_items",
-                                  "items": {
-                                    "type": "button",
-                                    "level": "link",
-                                    "actionType": "link",
-                                    "link": "${path}",
-                                    "body": [{
-                                      "type": "tpl",
-                                      "wrapperComponent": "span",
-                                      "className": "app-launcher-link slds-text-link--reset slds-app-launcher__tile--small slds-truncate creator-object-nav-${id}",
-                                      "tpl": "<span class=\"slds-truncate slds-text-link\">${name}</span>",
-                                    }],
-                                    "onEvent": {
-                                      "click": {
-                                        "actions": [
-                                          {
-                                            "actionType": "closeDialog"
-                                          }
-                                        ]
-                                      }
-                                    },
-                                    "inline": true,
-                                    "style": {
-                                    },
-                                    "className": "slds-col--padded slds-p-vertical_xx-small slds-size_1-of-5 slds-grow-none oneAppLauncherItem"
-                                  },
-                                  "className": "slds-grid slds-wrap"
-                                }
-                              ],
-                              "className": ""
-                            }
-                          ],
-                        }
-                      ],
-                      "className": "",
-                      "visibleOn": "",
-                      "clearValueOnHidden": false,
-                      "visible": true,
-                      "messages": {
-                      },
-                      "api": {
-                        "method": "get",
-                        "url": "${context.rootUrl}/service/api/apps/menus?mobile="+mobile,
-                        "data": null,
-                        "headers": {
-                          "Authorization": "Bearer ${context.tenantId},${context.authToken}"
-                        },
-                        "adaptor": "\nlet app_items = payload;\nlet object_items = [];\nlet objects = [];\napp_items.forEach((item) => {\n  item.children.forEach((i) => {\n    if (objects.indexOf(i.id) < 0) {\n      objects.push(i.id);\n      object_items.push(i)\n    }\n  })\n})\npayload.data = {\n  app_items,\n  object_items\n}\nreturn payload;"
-}
-                    }
-                  ]
-                },
-                "id": "u:b5dc095e1c11"
+  const formFactor = window.innerWidth < 768 ? 'SMALL' : 'LARGE';
+  const isMobile = formFactor === "SMALL" ? true : false;
+  let dialogSchema = {}
+  if(isMobile){
+    dialogSchema = {
+      "type": "each",
+      "name": "app_items",
+      "items": {
+        "type": "button",
+        "level": "link",
+        "actionType": "link",
+        "link": "${path}",
+        "body": [
+          {
+            "type": "tpl",
+            "tpl": "<div class='flex flex-col justify-center'><div class='text-center'><svg class='w-12 h-12 slds-icon slds-icon_container slds-icon-standard-${REPLACE(icon, '_', '-' )}' aria-hidden='true'><use xlink:href='/assets/icons/standard-sprite/svg/symbols.svg#${icon}'></use></svg></div><div class='text-center text-lg'>${name}</div></div>",
+            "badge": {
+              "mode": "text",
+              "text": "${value.${id}  | toInt}",
+              "overflowCount": 99,
+              "style": {
+                "right": "50%",
+                "margin-right": "-23px",
+                "height": "20px",
+                "border-radius": "10px",
+                "font-size": "16px",
+                "line-height": "18px"
+              }
             }
+          }
         ],
+        "onEvent": {
+          "click": {
+            "actions": [
+              {
+                "actionType": "closeDialog"
+              }
+            ]
+          }
+        },
+        "className": "block w-1/3 py-4",
+        "style": {
+          "display": "inline-flex",
+          "justify-content": "center"
+        }
+      },
+      "className": "flex flex-wrap",
+      "id": "u:a98e9f6fb4db"
     }
+  }else{
+    dialogSchema = {
+      "type": "service",
+      "id": "u:0f6224a0836f",
+      "affixFooter": false,
+      "body": [
+        {
+          "type": "collapse-group",
+          "activeKey": [
+            "1",
+            "2"
+          ],
+          "body": [
+            {
+              "type": "collapse",
+              "key": "1",
+              "header": "所有应用",
+              "body": [
+                {
+                  "type": "each",
+                  "name": "app_items",
+                  "items": {
+                    "type": "button",
+                    "level": "link",
+                    "actionType": "link",
+                    "link": "${path}",
+                    "body": [{
+                      "type": "tpl",
+                      "tpl": "<div class='slds-app-launcher__tile slds-text-link_reset'><div class='slds-app-launcher__tile-figure'><svg class='w-12 h-12 slds-icon slds-icon_container slds-icon-standard-${REPLACE(icon, '_', '-')}' aria-hidden='true'><use xlink:href='/assets/icons/standard-sprite/svg/symbols.svg#${icon}'></use></svg><span class='slds-assistive-text'>${name}</span></div><div class='slds-app-launcher__tile-body'><span class='slds-link text-blue-600 text-lg'><span title='${name}'>${name}</span></span><div style='display: -webkit-box; -webkit-line-clamp: 1;-webkit-box-orient: vertical;overflow: hidden;'><span title='${description}'>${description}</span></div></div></div>",
+                      "badge": {
+                        "mode": "text",
+                        "text": "${value.${id} | toInt}",
+                        "className": "w-full",
+                        "overflowCount": 99,
+                        "style": {
+                          "top": "20px",
+                          "left": "37px",
+                          "height": "20px",
+                          "border-radius": "10px",
+                          "line-height": "18px",
+                          "margin-left": "${value.${id}>9?(value.${id}>99?'-21px':'-11px'):'0'}",
+                          "right": "auto",
+                          "font-size": "16px"
+                        }
+                      }
+                    }],
+                    "onEvent": {
+                      "click": {
+                        "actions": [
+                          {
+                            "actionType": "closeDialog"
+                          }
+                        ]
+                      }
+                    },
+                    "inline": true,
+                    "style": {
+                    },
+                    "className": "slds-p-horizontal_small slds-size_1-of-1 slds-medium-size_1-of-3"
+                  },
+                  "className": "slds-grid slds-wrap slds-grid_pull-padded"
+                }
+              ]
+            },
+            {
+              "type": "collapse",
+              "key": "2",
+              "header": "所有项目",
+              "body": [
+                {
+                  "type": "each",
+                  "name": "object_items",
+                  "items": {
+                    "type": "button",
+                    "level": "link",
+                    "actionType": "link",
+                    "link": "${path}",
+                    "body": [{
+                      "type": "tpl",
+                      "wrapperComponent": "span",
+                      "className": "app-launcher-link slds-text-link--reset slds-app-launcher__tile--small slds-truncate creator-object-nav-${id}",
+                      "tpl": "<span class=\"slds-truncate slds-text-link\">${name}</span>",
+                    }],
+                    "onEvent": {
+                      "click": {
+                        "actions": [
+                          {
+                            "actionType": "closeDialog"
+                          }
+                        ]
+                      }
+                    },
+                    "inline": true,
+                    "style": {
+                    },
+                    "className": "slds-col--padded slds-p-vertical_xx-small slds-size_1-of-5 slds-grow-none oneAppLauncherItem"
+                  },
+                  "className": "slds-grid slds-wrap"
+                }
+              ],
+              "className": ""
+            }
+          ],
+        }
+      ],
+      "className": "",
+      "visibleOn": "",
+      "clearValueOnHidden": false,
+      "visible": true,
+      "messages": {
+      },
+      "api": {
+        "method": "get",
+        "url": "${context.rootUrl}/service/api/apps/menus?mobile=" + isMobile,
+        "data": null,
+        "headers": {
+          "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+        },
+        "adaptor": "\nlet app_items = payload;\nlet object_items = [];\nlet objects = [];\napp_items.forEach((item) => {\n  item.children.forEach((i) => {\n    if (objects.indexOf(i.id) < 0) {\n      objects.push(i.id);\n      object_items.push(i)\n    }\n  })\n})\npayload.data = {\n  app_items,\n  object_items\n}\nreturn payload;"
+      }
+    }
+  }
+
+  return {
+    "type": "service",
+    "affixFooter": false,
+    className,
+    "body": [
+      {
+        "type": "service",
+        "body": [
+          {
+            "type": "button",
+            "actionType": "dialog",
+            "className": "flex items-center",
+            "body": [
+              {
+                "type": "tpl",
+                "className": "flex items-center",
+                "tpl": "<div aria-haspopup='true' title='Open App Launcher' class='slds-icon-waffle_container slds-context-bar__button' title='Open App Launcher' type='button'><span class='slds-icon-waffle'><span class='slds-r1'></span><span class='slds-r2'></span><span class='slds-r3'></span><span class='slds-r4'></span><span class='slds-r5'></span><span class='slds-r6'></span><span class='slds-r7'></span><span class='slds-r8'></span><span class='slds-r9'></span></span></div>",
+                "badge": {
+                  "visibleOn": "${value.workflow | toInt}",
+                  "offset": [3, -3],
+                  "style": {
+                    "width": "8px",
+                    "height": "8px"
+                  }
+                }
+              },
+              {
+                type: 'tpl',
+                className: `text-xl ml-4 mr-4 text-black nowrap ${appNameClassName} `,
+                tpl: '${app.name}',
+                hiddenOn: `${!!app && !!!showAppName}`
+              }
+            ],
+            "dialog": {
+              "size": "xl",
+              "title": {
+                "type": "tpl",
+                "tpl": "应用程序启动器",
+                "className": "block text-xl text-center"
+              },
+              "actions": [
+              ],
+              "body": [
+                dialogSchema
+              ]
+            },
+            "id": "u:b5dc095e1c11"
+          }
+        ],
+        "id": "u:06ee48db134a",
+        "messages": {
+        },
+        "api": {
+          "method": "post",
+          "url": "${context.rootUrl}/graphql",
+          "headers": {
+            "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+          },
+          "adaptor": "if (payload.data.steedos_keyvalues.length > 0) {\n  payload.data.value = payload.data.steedos_keyvalues[0].value\n  for (i in payload.data.value){\n    if (payload.data.value[i] <= 0) {\n      delete payload.data.value[i]\n    }\n  }\n}\nreturn payload",
+          "messages": {
+          },
+          "data": {
+            "query": "{steedos_keyvalues{value}}",
+            "userId": "${context.userId}",
+            "tenantId": "${context.tenantId}"
+          },
+          "requestAdaptor": "var query = `{\n  steedos_keyvalues\n  (filters: [[\"user\",\"=\", \"`+ api.data.userId + `\"], [\"space\",\"=\", \"` + api.data.tenantId+`\"], [\"key\", \"=\", \"badge\"]])\n  {\n  value\n  }\n}`\napi.data = {}\napi.data.query=query\nreturn api;"
+        }
+      }
+    ],
+    "clearValueOnHidden": false,
+    "visible": true,
+    "messages": {
+    },
+    "api": {
+      "method": "get",
+      "url": "${context.rootUrl}/service/api/apps/menus?mobile=true",
+      "headers": {
+        "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+      },
+      "adaptor": "\nlet app_items = payload;\npayload.data = {\n  app_items\n}\nreturn payload;",
+      "messages": {
+      }
+    },
+    "id": "u:2c8bd22d4ea8"
+  }
+
 }
