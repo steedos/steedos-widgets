ac36d1185c5edd89231c47228b2b2520820061bf
何夏鹏 <126241326+hexiapengCom@users.noreply.github.com>
2023-03-17 15:46:03 +0800

    #4491 优化了一下代码

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
@@ -49,6 +49,62 @@ const filterService = filterForm.context.getComponents().find(function(n){
 filterService.setData({showFieldsFilter: !!!filterService.props.data.showFieldsFilter});
 `;
 
+function getExportApiRequestAdaptorScript(){
+  return `
+    console.log(api.url);
+    // 获取列表视图的属性
+    let uiSchema = api.body.uiSchema;
+    let list_views = uiSchema.list_views;
+    let list_views_name = api.body.listName;
+    let col = list_views[list_views_name].columns;
+    let sort_test = list_views[list_views_name].sort;
+    
+    // 获取下载字段
+    let select = [];
+    _.each(col, (col) => {
+        if (col.field == undefined)
+            select.push(col);
+        else select.push(col.field);
+    });
+    
+    // 获取排序字段
+    
+    let sort = [];
+    _.forEach(sort_test, (sortField) => {
+        if (sortField.field_name == undefined)
+            sort.push(sortField);
+        else sort.push([sortField.field_name, sortField.order]);
+    })
+    
+    let orders = [];
+    _.map(sort, (value) => {
+        let order_tmp = [];
+        if (value[1] == "desc")
+            order_tmp = value[0] + ' desc';
+        else
+            order_tmp = value[0];
+        orders.push(order_tmp);
+    });
+    let order = orders.join(',');
+    
+    let filename = uiSchema.label + "-" + list_views[list_views_name].label;
+    
+    url_tmp = api.url.split('?')[0];
+    api.url = url_tmp + "?$select=" + select.toString() + "&filename=" + filename;
+    
+    // 判断sort 和 filters
+    if (sort.length > 0) {
+        api.url += "&$orderby=" + order;
+    }
+    let filters = list_views[list_views_name].filters;
+    if (filters && filters.length > 0) {
+        api.url = api.url + "&filters=" + JSON.stringify(filters);
+    }
+    console.log(api.url);
+    return api;
+  `
+}
+
 export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = false, hiddenCount = false} = {}){
 
   if(formFactor === 'SMALL'){
@@ -147,59 +203,7 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                     "url": "${context.rootUrl}/api/record/export/${objectName}",
                     "method": "get",
                     "messages": {},
-                    "requestAdaptor": `
-                      console.log(api.url);
-                      // 获取列表视图的属性
-                      let uiSchema = api.body.uiSchema;
-                      let list_views = uiSchema.list_views;
-                      let list_views_name = api.body.listName;
-                      let col = list_views[list_views_name].columns;
-                      let sort_test = list_views[list_views_name].sort;
-                      
-                      // 获取下载字段
-                      let select = [];
-                      _.each(col, (col) => {
-                          if (col.field == undefined)
-                              select.push(col);
-                          else select.push(col.field);
-                      });
-                      
-                      // 获取排序字段
-                      
-                      let sort = [];
-                      _.forEach(sort_test, (sortField) => {
-                          if (sortField.field_name == undefined)
-                              sort.push(sortField);
-                          else sort.push([sortField.field_name, sortField.order]);
-                      })
-                      
-                      let orders = [];
-                      _.map(sort, (value) => {
-                          let order_tmp = [];
-                          if (value[1] == "desc")
-                              order_tmp = value[0] + ' desc';
-                          else
-                              order_tmp = value[0];
-                          orders.push(order_tmp);
-                      });
-                      let order = orders.join(',');
-                      
-                      let filename = uiSchema.label + "-" + list_views[list_views_name].label;
-                      
-                      url_tmp = api.url.split('?')[0];
-                      api.url = url_tmp + "?$select=" + select.toString() + "&filename=" + filename;
-                      
-                      // 判断sort 和 filters
-                      if (sort.length > 0) {
-                          api.url += "&$orderby=" + order;
-                      }
-                      let filters = list_views[list_views_name].filters;
-                      if (filters && filters.length > 0) {
-                          api.url = api.url + "&filters=" + JSON.stringify(filters);
-                      }
-                      console.log(api.url);
-                      return api;
-                      `,
+                    "requestAdaptor": `${getExportApiRequestAdaptorScript()}`,
                     "data": {
                       "uiSchema": "${uiSchema}",
                       "listName": "${listName}"
