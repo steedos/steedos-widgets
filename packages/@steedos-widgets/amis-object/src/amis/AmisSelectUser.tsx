fcb31210b6a375b79f30ecd484b776041e7b38e7
殷亮辉 <yinlianghui@hotoa.com>
2023-03-19 22:07:28 +0800

    选人steedos-select-user组件需要支持required必填功能
    https://github.com/steedos/steedos-platform/issues/4538

--- packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
@@ -2,15 +2,15 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2023-03-17 16:54:34
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2023-03-19 15:49:15
+ * @LastEditTime: 2023-03-19 22:05:51
  * @FilePath: /steedos-ee/Users/yinlianghui/Documents/GitHub/steedos-webapp-nextjs/packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { getSelectUserSchema, getIdsPickerSchema } from '@steedos-widgets/amis-lib';
+import { getSelectUserSchema, getIdsPickerSchema, Field } from '@steedos-widgets/amis-lib';
 
 export const AmisSelectUser = async (props) => {
   console.log(`AmisSelectUser props`, props)
-  const { $schema, data, ids, idsTrackOn } = props;
+  const { $schema, data, ids, idsTrackOn, readonly = false, ctx = {} } = props;
   let amisSchema: any;
   if(ids || idsTrackOn){
     amisSchema = await getIdsPickerSchema({
@@ -26,13 +26,22 @@ export const AmisSelectUser = async (props) => {
     });
   }
   else{
-    amisSchema = await getSelectUserSchema({
-      name: $schema.name,
-      multiple: $schema.multiple,
-      searchable: $schema.searchable,
-      onEvent: $schema.onEvent,
-      filters: $schema.filters
-    }, false, {});
+    try {
+      const steedosField = {
+        type: "lookup", 
+        reference_to: "users",
+        name: $schema.name,
+        multiple: $schema.multiple,
+        required: true,
+        searchable: $schema.searchable,
+        onEvent: $schema.onEvent,
+        filters: $schema.filters
+      };
+      amisSchema = await Field.convertSFieldToAmisField(steedosField, readonly, ctx);
+      // console.log(`amisSchema===`, amisSchema)
+    } catch (error) {
+        console.log(`error`, error)
+    }
   }
   amisSchema.name = $schema.name;
   amisSchema.label = $schema.label;
