c1121f50ff2ee39834329d5bd3db5f3009f494eb
殷亮辉 <yinlianghui@hotoa.com>
2023-03-22 17:23:12 +0800

    选人组件支持配置amis属性重写字段amis配置 #314

--- packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2023-03-17 16:54:34
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2023-03-22 15:04:02
+ * @LastEditTime: 2023-03-22 17:19:48
  * @FilePath: /steedos-ee/Users/yinlianghui/Documents/GitHub/steedos-webapp-nextjs/packages/@steedos-widgets/amis-object/src/amis/AmisSelectUser.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,7 @@ import { getSelectUserSchema, getIdsPickerSchema, Field } from '@steedos-widgets
 
 export const AmisSelectUser = async (props) => {
   // console.log(`AmisSelectUser props`, props)
-  const { $schema, data, ids, idsTrackOn, readonly = false, selectFirst = false, ctx = {} } = props;
+  const { $schema, data, ids, idsTrackOn, readonly = false, selectFirst = false, amis, ctx = {} } = props;
   const value = $schema.value;
   let amisSchema: any;
   try {
@@ -18,11 +18,13 @@ export const AmisSelectUser = async (props) => {
       type: "lookup",
       reference_to: "users",
       name: $schema.name,
+      label: $schema.label,
       multiple: $schema.multiple,
       required: $schema.required,
       searchable: $schema.searchable,
       onEvent: $schema.onEvent,
-      filters: $schema.filters
+      filters: $schema.filters,
+      amis
     };
     const options = Object.assign({}, ...ctx, { ids, idsTrackOn, selectFirst });
     if(value){
@@ -32,7 +34,5 @@ export const AmisSelectUser = async (props) => {
   } catch (error) {
     console.log(`error`, error)
   }
-  amisSchema.name = $schema.name;
-  amisSchema.label = $schema.label;
   return amisSchema;
 }
