e32caa0d9d940f155bcbd4a10331731bb9413153
殷亮辉 <yinlianghui@hotoa.com>
2023-03-23 11:21:48 +0800

    日历组件添加支持list模式 #306

--- packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
+++ packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: 殷亮辉 yinlianghui@hotoa.com
+ * @Date: 2023-03-22 09:31:21
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2023-03-23 11:16:06
+ * @FilePath: /steedos-widgets/packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react'
 import FullCalendarReact, { formatDate } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -68,7 +76,7 @@ export const FullCalendar = ({
       headerToolbar={{
         left: 'title',
         center: '',
-        right: 'prev,next today dayGridMonth,timeGridWeek,timeGridDay'
+        right: 'prev,next today dayGridMonth,timeGridWeek,timeGridDay,listWeek'
       }}
       locales={allLocales}
       locale={initialLocaleCode}
