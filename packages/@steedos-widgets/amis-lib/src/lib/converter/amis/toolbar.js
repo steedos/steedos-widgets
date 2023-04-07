61f57352342e7ad54695a9063fdc7b79beabd24c
何夏鹏 <126241326+hexiapengCom@users.noreply.github.com>
2023-04-07 17:54:12 +0800

    修改了一些小样式 #295

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
@@ -388,7 +388,6 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                                   "actions": [
                                     {
                                       "args": {
-                                        // 直接使用recordId不能拿到数据，只能通过result里面拿数据
                                         "url": "${context.rootUrl}/app/${appId}/${object_name}/grid/listview_${result.data.recordId|lowerCase}",
                                         "blank": false
                                       },
@@ -472,7 +471,7 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                       {
                         "dialog": {
                           "type": "dialog",
-                          "title": "共享设置 列表视图",
+                          "title": "共享设置",
                           "body": [
                             {
                               "type": "steedos-object-form",
@@ -509,7 +508,7 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                       {
                         "dialog": {
                           "type": "dialog",
-                          "title": "过滤设置 列表视图",
+                          "title": "过滤设置",
                           "body": [
                             {
                               "type": "steedos-object-form",
@@ -532,7 +531,6 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                                   "actions": [
                                     {
                                       "args": {
-                                        // 直接使用recordId不能拿到数据，只能通过result里面拿数据
                                         "url": "${context.rootUrl}/app/${appId}/${object_name}/grid/${listName}",
                                         "blank": false
                                       },
@@ -567,7 +565,7 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                         "args": {},
                         "dialog": {
                           "type": "dialog",
-                          "title": "显示的列 列表视图",
+                          "title": "显示的列",
                           "body": [
                             {
                               "type": "steedos-object-form",
@@ -576,7 +574,7 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                               "recordId": "${uiSchema.list_views[listName]._id}",
                               "className": "sm:border sm:shadow sm:rounded sm:border-gray-300 bg-white p-4",
                               "mode": "edit",
-                              "fieldsExtend": "{\n  \"columns\": {\n    \"amis\": {\n      \"type\": \"transfer\",\n  \"inputClassName\": \"m-b-lg\",\n       \"source\": {\n        \"method\": \"get\",\n        \"url\": \"${context.rootUrl}/service/api/amis-metadata-objects/objects/${objectName}/fields/options\",\n        \"headers\": {\n          \"Authorization\": \"Bearer ${context.tenantId},${context.authToken}\"\n        }\n      }\n    }\n  },\n  \"mobile_columns\": {\n    \"amis\": {\n      \"type\": \"transfer\",\n      \"source\": {\n        \"method\": \"get\",\n        \"url\": \"${context.rootUrl}/service/api/amis-metadata-objects/objects/${object_name}/fields/options\",\n        \"headers\": {\n          \"Authorization\": \"Bearer ${context.tenantId},${context.authToken}\"\n        }\n      }\n    }\n  }\n\n}",
+                              "fieldsExtend": "{\n  \"columns\": {\n    \"amis\": {\n      \"type\": \"transfer\",\n  \"source\": {\n        \"method\": \"get\",\n        \"url\": \"${context.rootUrl}/service/api/amis-metadata-objects/objects/${objectName}/fields/options\",\n        \"headers\": {\n          \"Authorization\": \"Bearer ${context.tenantId},${context.authToken}\"\n        }\n      }\n    }\n  },\n  \"mobile_columns\": {\n \"group\": \"手机端\",\n   \"amis\": {\n      \"type\": \"transfer\",\n      \"source\": {\n        \"method\": \"get\",\n        \"url\": \"${context.rootUrl}/service/api/amis-metadata-objects/objects/${object_name}/fields/options\",\n        \"headers\": {\n          \"Authorization\": \"Bearer ${context.tenantId},${context.authToken}\"\n        }\n      }\n    }\n  }\n\n}",
                               "initApiAdaptor": "const recordId_tmp = api.body.recordId;\nlet columns_tmp = {}, mobile_columns_tmp = {};\nif (recordId_tmp) {\n  columns_tmp = payload.data.initialValues.columns;\n  mobile_columns_tmp = payload.data.initialValues.mobile_columns;\n  if (columns_tmp) {\n    columns_tmp = lodash.map(columns_tmp, 'field');\n  }\n  if (mobile_columns_tmp) {\n    mobile_columns_tmp = lodash.map(mobile_columns_tmp, 'field');\n  }\n}\npayload.data.initialValues.columns = columns_tmp;\npayload.data.initialValues.mobile_columns = mobile_columns_tmp;\n\ndelete payload.extensions;\nreturn payload;",
                               "apiRequestAdaptor": "const formData_tmp = api.body.$;\nconst objectName_tmp = api.body.objectName;\nconst recordId_tmp = api.body.recordId;\n\nif (typeof formData_tmp.columns == 'string') {\n  formData_tmp.columns = formData_tmp.columns?.split(',');\n}\nif (typeof formData_tmp.mobile_columns == 'string') {\n  formData_tmp.mobile_columns = formData_tmp.mobile_columns?.split(',');\n}\n\n// 数据格式转换\nformData_tmp.columns = lodash.map(formData_tmp.columns, (item) => {\n  return { field: item };\n});\nformData.mobile_columns = lodash.map(formData.mobile_columns, (item) => {\n  return { field: item };\n});\n\n// 字符串拼接（不支持ES6语法）\nlet query_tmp = 'mutation{record: ' + objectName_tmp + '__insert(doc: {__saveData}){_id}}';\nif (api.body.recordId) {\n  query_tmp = 'mutation{record: ' + objectName_tmp + '__update(id: \"' + recordId_tmp +'\", doc: {__saveData}){_id}}';\n};\ndelete formData_tmp._id;\nlet __saveData_tmp = JSON.stringify(JSON.stringify(formData_tmp));\napi.data = { query: query_tmp.replace('{__saveData}', __saveData_tmp) };\n\nreturn api;",
                               "fields": [
@@ -589,7 +587,6 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                                   "actions": [
                                     {
                                       "args": {
-                                        // 直接使用recordId不能拿到数据，只能通过result里面拿数据
                                         "url": "${context.rootUrl}/app/${appId}/${object_name}/grid/${listName}",
                                         "blank": false
                                       },
