72789799e7a72e8b34bbf9f0683d417a62702160
何夏鹏 <126241326+hexiapengCom@users.noreply.github.com>
2023-04-12 10:33:14 +0800

    #295 删除后重刷整个页面

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
@@ -639,27 +639,43 @@ export function getObjectHeaderToolbar(mainObject, formFactor, {showDisplayAs = 
                 "label": "删除",
                 "visibleOn": "${(global.user.is_space_admin || global.userId == uiSchema.list_views[listName].owner) && !ARRAYINCLUDES(['all', 'recent', 'my'], listName) }",
                 "confirmText": "如果您删除此列表视图，该视图将为所有具备访问权限的用户永久删除。是否确定要删除？",
-                "api": {
-                  "url": "${context.rootUrl}/graphql",
-                  "method": "post",
-                  "headers": {
-                    "Authorization": "Bearer ${context.tenantId},${context.authToken}"
-                  },
-                  "data": {
-                    "&": "$$",
-                    "uiSchema": "${uiSchema}",
-                    "recordId": "${uiSchema.list_views[listName]._id}"
-                  },
-                  "messages": {
-                    "success": "删除成功",
-                    "failed": "删除失败"
-                  },
-                  "requestAdaptor": "const { recordId } = api.body;\nvar deleteArray = [];\nif (recordId) { deleteArray.push(`delete:object_listviews__delete(id: \"${recordId}\")`); }\napi.data = { query: `mutation{${deleteArray.join(',')}}` };\n  return api;\n",
-                  "adaptor": "if (payload.errors) {\n  payload.status = 2;\n  payload.msg = payload.errors[0].message;\n}\nreturn payload;",
+                "onEvent": {
+                  "click": {
+                    "actions": [
+                      {
+                        "actionType": "ajax",
+                        "args": {
+                          "api": {
+                            "url": "${context.rootUrl}/graphql",
+                            "method": "post",
+                            "headers": {
+                              "Authorization": "Bearer ${context.tenantId},${context.authToken}"
+                            },
+                            "data": {
+                              "&": "$$",
+                              "uiSchema": "${uiSchema}",
+                              "recordId": "${uiSchema.list_views[listName]._id}"
+                            },
+                            "messages": {
+                              "success": "删除成功"
+                            },
+                            "requestAdaptor": "const { recordId } = api.body;\nvar deleteArray = [];\nif (recordId) { deleteArray.push(`delete:object_listviews__delete(id: \"${recordId}\")`); }\napi.data = { query: `mutation{${deleteArray.join(',')}}` };\n  return api;\n",
+                            "adaptor": "if (payload.errors) {\n  payload.status = 2;\n  payload.msg = payload.errors[0].message;\n}\nreturn payload;",
+                          }
+                        }
+                      },
+                      {
+                        "actionType": "url",
+                        "args": {
+                          "url": "${context.rootUrl}/app/${appId}/${objectName}/grid/all",
+                          "blank": false
+                        },
+                        "expression": "data.delete == 1"
+                      }
+                    ]
+                  }
                 },
-                "redirect": "./all",
-                "actionType": "ajax",
-                "outputVar": "responseResult",
+
               },
               {
                 "type": "button",
