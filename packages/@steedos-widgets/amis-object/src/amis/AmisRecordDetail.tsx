8f47d5f0222b45f268d9766cbfecc34ab7774c9e
tujiajun <tujiajun@steedos.com>
2023-04-23 15:13:38 +0800

    [Bug]: 日程对象的日历视图中点击任意的日程，打开的记录详情弹框未显示具体内容，并报错 #405

--- packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetail.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetail.tsx
@@ -7,9 +7,10 @@
  */
 
 import { getRecordDetailSchema } from '@steedos-widgets/amis-lib'
+import { has } from 'lodash';
 
 export const AmisRecordDetail = async (props) => {
-    console.log(`AmisRecordDetail======>`, props)
+    // console.log(`AmisRecordDetail======>`, props)
     const { className, $schema, appId, objectApiName = "space_users", recordId, onEvent, defaultData, body } = props;
     const schema = (await getRecordDetailSchema(objectApiName, appId)).amisSchema;
     // 在非记录页组件下全局作用域下无recordId，会导致表单接口sendOn始终为false，无法发起请求。
@@ -18,11 +19,15 @@ export const AmisRecordDetail = async (props) => {
     //   recordDetailData.recordId = recordId;
     // }
     // schema.data = Object.assign({}, schema.data, recordDetailData);
+
+    if(has(props, "recordId") && $schema.recordId !== "${recordId}"){
+      schema.data.recordId = props.recordId;
+    }
     schema.className = className;
 
     if(body){
       schema.body = body;
     }
-    console.log(`AmisRecordDetail====schema==>`, schema)
+    // console.log(`AmisRecordDetail====schema==>`, schema)
     return schema
   }
