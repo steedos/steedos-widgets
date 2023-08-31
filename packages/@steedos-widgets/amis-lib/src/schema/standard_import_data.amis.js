342aaa23b88cd7c1569fc1e91d510faeb1477430
殷亮辉 <yinlianghui@hotoa.com>
2023-08-31 14:26:25 +0800

    数据导入界面优化，使用steedos-field组件而不是原生 amis
    https://github.com/steedos/steedos-platform/issues/5305

--- packages/@steedos-widgets/amis-lib/src/schema/standard_import_data.amis.js
+++ packages/@steedos-widgets/amis-lib/src/schema/standard_import_data.amis.js
@@ -67,175 +67,39 @@ export const getSchema = (uiSchema) => {
                           collapsable: true,
                           body: [
                             {
-                              name: "object_name",
-                              label: i18next.t('frontend_import_data_object_name'),
-                              required: false,
-                              type: "select",
-                              joinValues: false,
-                              extractValue: true,
-                              autoComplete: {
-                                method: "post",
-                                url: "${context.rootUrl}/graphql",
-                                data: {
-                                  orderBy: "${orderBy}",
-                                  orderDir: "${orderDir}",
-                                  pageNo: "${page}",
-                                  pageSize: "${perPage}",
-                                  query:
-                                    "{options:objects(filters: {__filters}, top: {__top}){_id label:label value:name},count:objects__count(filters:{__filters})}",
-                                  $term: "$term",
-                                  $value: "$object_name.name",
-                                  $: "$$",
-                                  rfield: "${object_name}",
-                                },
-                                headers: {
-                                  Authorization:
-                                    "Bearer ${context.tenantId},${context.authToken}",
-                                },
-                                requestAdaptor:
-                                  "\n        var filters = '[]';\n        var top = 10;\n        if(api.data.$term){\n            filters = '[\"name\", \"contains\", \"'+ api.data.$term +'\"]';\n        }else if(api.data.$value){\n            filters = '[\"_id\", \"=\", \"'+ api.data.$value +'\"]';\n        }\n        api.data.query = api.data.query.replace(/{__filters}/g, filters).replace('{__top}', top);\n        return api;\n    ",
-                              },
-                              className: "m-1",
-                              labelClassName: "text-left",
-                              clearValueOnHidden: false,
-                              id: "u:04295fee3896",
-                              multiple: false,
-                              hidden: true,
+                              "type": "steedos-field",
+                              "config": {
+                                  "type": "lookup",
+                                  "label": i18next.t('frontend_import_data_object_name'),
+                                  "reference_to": "objects",
+                                  "name": "object_name",
+                                  "required": true,
+                                  "amis":{
+                                    "hidden": true
+                                  }
+                              }
                             },
                             {
-                              name: "queue_import",
-                              label: i18next.t('frontend_import_data_queue_import'),
-                              required: true,
-                              type: "picker",
-                              labelField: "description",
-                              valueField: "_id",
-                              modalMode: "dialog",
-                              source: {
-                                method: "post",
-                                url: "${context.rootUrl}/graphql",
-                                data: {
-                                  orderBy: "${orderBy}",
-                                  orderDir: "${orderDir}",
-                                  pageNo: "${page}",
-                                  pageSize: "${perPage}",
-                                  query:
-                                    '{rows:queue_import(filters: {__filters}, top: {__top}, skip: {__skip}, sort: "{__sort}"){_id,description,object_name,encoding,template_url,_display:_ui{object_name,encoding}},count:queue_import__count(filters:{__filters})}',
-                                  $term: "$term",
-                                  $self: "$$",
-                                },
-                                headers: {
-                                  Authorization:
-                                    "Bearer ${context.tenantId},${context.authToken}",
-                                },
-                                requestAdaptor:
-                                  "\n        const selfData = JSON.parse(JSON.stringify(api.data.$self));\n        var filters = [];\n        var pageSize = api.data.pageSize || 10;\n        var pageNo = api.data.pageNo || 1;\n        var skip = (pageNo - 1) * pageSize;\n        var orderBy = api.data.orderBy || '';\n        var orderDir = api.data.orderDir || '';\n        var sort = orderBy + ' ' + orderDir;\n        var allowSearchFields = [\"description\"];\n        if(api.data.$term){\n            filters = [[\"name\", \"contains\", \"'+ api.data.$term +'\"]];\n        }else if(selfData.op === 'loadOptions' && selfData.value){\n            if(selfData.value && selfData.value.indexOf(',') > 0){\n                filters = [[\"_id\", \"=\", selfData.value.split(',')]];\n            }else{\n                filters = [[\"_id\", \"=\", selfData.value]];\n            }\n        }\n        if(allowSearchFields){\n            allowSearchFields.forEach(function(key){\n                const keyValue = selfData[key];\n                if(keyValue){\n                    filters.push([key, \"contains\", keyValue]);\n                }\n            })\n        }\n\n        const filtersFunction = function(filters, values){return ['object_name', '=', values.object_name]};\n\n        if(filtersFunction){\n            const _filters = filtersFunction(filters, api.data.$self.__super.__super);\n            if(_filters && _filters.length > 0){\n                filters.push(_filters);\n            }\n        }\n\n        api.data.query = api.data.query.replace(/{__filters}/g, JSON.stringify(filters)).replace('{__top}', pageSize).replace('{__skip}', skip).replace('{__sort}', sort.trim());\n        return api;\n    ",
-                                adaptor:
-                                  "\n    const enable_tree = undefined;\n    if(enable_tree){\n        const records = payload.data.rows;\n        const treeRecords = [];\n        const getChildren = (records, childrenIds)=>{\n            if(!childrenIds){\n                return;\n            }\n            const children = _.filter(records, (record)=>{\n                return _.includes(childrenIds, record._id)\n            });\n            _.each(children, (item)=>{\n                if(item.children){\n                    item.children = getChildren(records, item.children)\n                }\n            })\n            return children;\n        }\n\n        _.each(records, (record)=>{\n            if(!record.parent){\n                treeRecords.push(Object.assign({}, record, {children: getChildren(records, record.children)}));\n            }\n        });\n        payload.data.rows = treeRecords;\n    }\n    return payload;\n    ",
-                              },
-                              size: "lg",
-                              pickerSchema: {
-                                mode: "table",
-                                name: "thelist",
-                                draggable: false,
-                                headerToolbar: ["reload"],
-                                defaultParams: {
-                                  perPage: 20,
-                                },
-                                columns: [
-                                  {
-                                    name: "_index",
-                                    type: "text",
-                                    width: 32,
-                                    placeholder: "",
-                                  },
-                                  {
-                                    name: "description",
-                                    label: i18next.t('frontend_import_data_queue_import_description'),
-                                    type: "tpl",
-                                    tpl: '<a target="_blank" href="/app/admin/queue_import/view/${_id}">${description}</a>',
-                                    className: "whitespace-nowrap",
-                                    html: null,
-                                  },
-                                  {
-                                    name: "object_name",
-                                    label: i18next.t('frontend_import_data_object_name'),
-                                    type: "tpl",
-                                    tpl: '<a target="_blank" href="/app/admin/${_display.object_name.objectName}/view/${_display.object_name.value}">${_display.object_name.label}</a>',
-                                    className: "whitespace-nowrap",
-                                    html: null,
-                                  },
-                                  {
-                                    name: "encoding",
-                                    label: i18next.t('frontend_import_data_queue_import_encoding'),
-                                    type: "tpl",
-                                    tpl: "<div>${_display.encoding}</div>",
-                                    className: "whitespace-nowrap",
-                                    html: null,
-                                  },
-                                  {
-                                    name: "template_url",
-                                    label: i18next.t('frontend_import_data_queue_import_template_url'),
-                                    type: "button-group",
-                                    buttons: [
-                                      {
-                                        type: "button",
-                                        label: i18next.t('frontend_download'),
-                                        visibleOn: "${template_url}",
-                                        level: "link",
-                                        actionType: "download",
-                                        api:{
-                                          url: "${template_url}",
-                                          method: "get",
-                                          headers: {
-                                            Authorization: "Bearer ${context.tenantId},${context.authToken}"
-                                          }
-                                        }
-                                      }
-                                    ],
-                                    options: {
-                                      html: true
-                                    },
-                                    className: "whitespace-nowrap",
-                                  },
-                                  null,
-                                ],
-                                syncLocation: false,
-                                keepItemSelectionOnPageChange: true,
-                                checkOnItemClick: true,
-                                autoFillHeight: false,
-                              },
-                              joinValues: false,
-                              extractValue: true,
-                              className: "m-1",
-                              labelClassName: "text-left",
-                              clearValueOnHidden: true,
-                              id: "u:401df27113e0",
+                              "type": "steedos-field",
+                              "config": {
+                                  "type": "lookup",
+                                  "label": i18next.t('frontend_import_data_queue_import_description'),
+                                  "reference_to": "queue_import",
+                                  "name": "queue_import",
+                                  "required": true
+                              }
                             },
                             {
-                              name: "file",
-                              label: i18next.t('frontend_import_data_file'),
-                              required: true,
-                              type: "input-file",
-                              accept: ".xlsx,.xls",
-                              className: "m-1",
-                              labelClassName: "text-left",
-                              clearValueOnHidden: true,
-                              useChunk: false,
-                              receiver: {
-                                method: "post",
-                                url: "${context.rootUrl}/s3/files",
-                                data: {
-                                  $: "$$",
-                                  context: "${context}",
-                                },
-                                adaptor:
-                                  '\n                const { context } = api.body; \n                var rootUrl = context.rootUrl + "/api/files/files/";\n                payload = {\n                    status: response.status == 200 ? 0 : response.status,\n                    msg: response.statusText,\n                    data: {\n                        value: payload._id,\n                        name: payload.original.name,\n                        url: rootUrl + payload._id,\n                    }\n                }\n                return payload;\n            ',
-                                headers: {
-                                  Authorization:
-                                    "Bearer ${context.tenantId},${context.authToken}",
-                                },
-                              },
-                              id: "u:0213f2cc365b",
+                              "type": "steedos-field",
+                              "config": {
+                                  "type": "file",
+                                  "label": i18next.t('frontend_import_data_file'),
+                                  "name": "file",
+                                  "required": true,
+                                  "amis": {
+                                    accept: ".xlsx,.xls"
+                                  }
+                              }
                             },
                           ],
                           id: "u:4899c260d667",
