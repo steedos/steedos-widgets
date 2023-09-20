feaab78e5b317874eed810fb013b1ace356b1e99
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-09-20 16:11:46 +0800

    去掉crud工具栏的背景。

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
@@ -783,7 +783,7 @@ export async function getTableSchema(fields, options){
     return {
         mode: "table",
         name: "thelist",
-        headerToolbarClassName: "py-2 px-2 border-gray-300 bg-gray-100 border-solid border-b",
+        headerToolbarClassName: "py-2 px-2 border-gray-300 border-solid border-b",
         className: "",
         draggable: false,
         defaultParams: getDefaultParams(options),
