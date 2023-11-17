ec533feab8829a8a6c4c08b14a37cc42dabdbe9a
涂佳俊 <tujiajun@steedos.com>
2023-11-17 16:38:36 +0800

    点击预览，如没有记录，列表界面标题丢失 #5779

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
@@ -476,9 +476,9 @@ function getFieldWidth(width){
 async function getTableColumns(fields, options){
     const columns = [];
     if(!options.isLookup){
-        columns.push({name: '_index',type: 'text', width: 32, placeholder: ""});
         //将_display放入crud的columns中，可以通过setvalue修改行内数据域的_display，而不影响上层items的_display,用于批量编辑
         columns.push({name: '_display',type: 'static', width: 32, placeholder: "",id: "_display_${_index}", className: "hidden"});
+        columns.push({name: '_index',type: 'text', width: 32, placeholder: ""});
     }
     const allowEdit = options.permissions?.allowEdit && !options.isLookup && options.enable_inline_edit != false;
     
