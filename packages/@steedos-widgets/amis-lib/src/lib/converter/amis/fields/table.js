c2f74ac93a5ea144457274941cb93b25c3c999e7
廖大雪 <2291335922@qq.com>
2023-12-04 17:29:05 +0800

    现在进入对象权限详情页不方便
    https://github.com/steedos/steedos-platform/issues/5914

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
@@ -624,8 +624,8 @@ export async function getTableColumns(fields, options){
         const href = Router.getObjectDetailPath({
             ...options,  formFactor: options.formFactor, appId: "${appId}", objectName: options.objectName || "${objectName}", recordId: `\${${options.idFieldName}}`
         })
-        columns[0].type = "tpl";
-        columns[0].tpl = `<a href="${href}">\${${columns[0].name}}</a>`
+        columns[1].type = "tpl";
+        columns[1].tpl = `<a href="${href}">\${${columns[1].name}}</a>`
     }
     return columns;
 }
