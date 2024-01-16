7c97cc94b79819a3e744978e898f0b74dbcc7707
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-01-16 14:57:06 +0800

    调整翻页可选项

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
@@ -1051,7 +1051,7 @@ export async function getTableSchema(fields, options){
             }
             return {
                 mode: "cards",
-                perPageAvailable: [5, 10, 20, 50, 100, 500],
+                perPageAvailable: [20, 50, 100, 500],
                 name: "thelist",
                 headerToolbarClassName: "py-2 px-2 border-gray-300 border-solid border-b",
                 className: "",
@@ -1084,7 +1084,7 @@ export async function getTableSchema(fields, options){
 
     return {
         mode: "table",
-        perPageAvailable: [5, 10, 20, 50, 100, 500],
+        perPageAvailable: [20, 50, 100, 500],
         name: "thelist",
         headerToolbarClassName: "py-2 px-2 border-gray-300 border-solid border-b",
         className: "",
