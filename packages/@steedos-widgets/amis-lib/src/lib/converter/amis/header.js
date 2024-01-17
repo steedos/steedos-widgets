60decfe5e60ef7fd2aab152353a915048bf48619
baozhoutao <baozhoutao@hotoa.com>
2024-01-17 10:37:41 +0800

    高亮字段适配更大的屏幕尺寸.

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/header.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/header.js
@@ -424,7 +424,7 @@ export async function getObjectRecordDetailHeader(objectSchema, recordId, option
 
     body.push({
       "type": "form",
-      "className": "p-4 bg-white compact-layouts",
+      "className": "p-4 bg-white compact-layouts grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 3xl:grid-cols-8 4xl:grid-cols-8 5xl:grid-cols-10",
       "wrapWithPanel": false,
       "actions": [],
       "body": details,
