c4553bd1df9bacdd5b2c12a15c3674eb3cd9447b
涂佳俊 <tujiajun@steedos.com>
2024-01-18 15:07:19 +0800

    列表视图，宽字段，默认换行
    https://github.com/steedos/steedos-platform/issues/6276

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/table.js
@@ -514,9 +514,13 @@ export async function getTableColumns(fields, options){
         const quickEditSchema = allowEdit ? await getQuickEditSchema(field, options) : allowEdit;
         let className = "";
         if(field.wrap != true){
-            className += " whitespace-nowrap "
+            if(field.wrap != false && field.is_wide){
+                className += " break-words ";
+            }else{
+                className += " whitespace-nowrap ";
+            }
         }else{
-            className += " break-words "
+            className += " break-words ";
         }
         let columnItem;
         if((field.is_name || field.name === options.labelFieldName) && options.objectName === 'cms_files'){
