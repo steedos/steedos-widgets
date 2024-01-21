32dfd0c9d3065fca08163cd832dc0fc72718cd73
殷亮辉 <yinlianghui@hotoa.com>
2024-01-21 10:56:52 +0800

    remove console.log

--- packages/@steedos-widgets/amis-object/src/amis/AmisInputTable.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisInputTable.tsx
@@ -2,15 +2,15 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2023-11-15 09:50:22
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-01-19 15:14:24
+ * @LastEditTime: 2024-01-21 10:56:30
  */
 import { getAmisInputTableSchema, i18next } from '@steedos-widgets/amis-lib'
 
 export const AmisInputTable = async (props) => {
-  console.log("AmisInputTable props", props);
+  // console.log("AmisInputTable props", props);
   const { $schema, fields, name, id, data, columns, amis, className, tableClassName, headerToolbar, footerToolbar, 
     inlineEditMode, strictMode, dialog, primaryKey, showOperation, fieldPrefix, autoGeneratePrimaryKeyValue } = props;
   const amisSchema = await getAmisInputTableSchema(props);
-  console.log("=AmisInputTable==amisSchema====", amisSchema);
+  // console.log("=AmisInputTable==amisSchema====", amisSchema);
   return amisSchema;
 }
