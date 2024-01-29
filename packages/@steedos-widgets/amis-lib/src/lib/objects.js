6f74dd400866357777f7b7ff18fcd049d24800c2
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-01-29 18:32:23 +0800

    简化列表页

--- packages/@steedos-widgets/amis-lib/src/lib/objects.js
+++ packages/@steedos-widgets/amis-lib/src/lib/objects.js
@@ -538,7 +538,7 @@ export async function getRecordDetailSchema(objectName, appId, props = {}){
     }
     const content = {
         "type": "tabs",
-        "className": "steedos-record-tabs bg-white p-4 m-0",
+        "className": "steedos-record-tabs bg-white p-4 m-0 border-b",
         "contentClassName": "bg-none",
         "tabs": [
             detailed
