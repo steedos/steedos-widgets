19745c0d2b7ac61388714bc8dfaa8557aaac77c1
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2024-11-14 17:59:13 +0800

    优化记录详情页

--- packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetailHeader.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisRecordDetailHeader.tsx
@@ -11,7 +11,7 @@ import { getRecordDetailHeaderSchema , getUISchema} from '@steedos-widgets/amis-
 export const AmisRecordDetailHeader = async (props) => {
   // console.log(`AmisRecordDetailHeader=====>`, props)
   //sticky在最新版ios上存在bug，因此暂时去除手机版sticky
-  const { className = 'sm:sticky top-0 sm:z-10 p-0 bg-white sm:m-4 sm:shadow-sm sm:border sm:rounded', schemaFilter, showButtons, showBackButton } = props;
+  const { className = 'sm:sticky top-0 sm:z-10 p-0 bg-white sm:m-4 sm:shadow sm:rounded', schemaFilter, showButtons, showBackButton } = props;
   const objectUiSchema = await getUISchema(props.objectApiName || "space_users", false);
   const defaultOnEvent = {}
   const { $schema, recordId, onEvent = defaultOnEvent, showRecordTitle = true } = props;
