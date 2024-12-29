b58cf6374612623ac40b74430cec3891a40a730a
殷亮辉 <yinlianghui@hotoa.com>
2024-12-29 18:03:34 +0800

    steedos object storybook objectform 显示为空白

--- apps/storybook/stories/steedos-object.amis.stories.tsx
+++ apps/storybook/stories/steedos-object.amis.stories.tsx
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-09 11:54:45
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-12-29 17:51:33
+ * @LastEditTime: 2024-12-29 18:02:15
  * @Description: 
  */
 import React, {useEffect, useState} from 'react';
@@ -24,16 +24,21 @@ if(typeof window != 'undefined' && !(window as any).SteedosUI){
 
 const assetUrls = process.env.STEEDOS_UNPKG_URL + '/@steedos-widgets/amis-object/dist/assets-dev.json'
 
+
+const rootUrl = process.env.ROOT_URL;
+const userId = process.env.STEEDOS_USERID;
+const tenantId = process.env.STEEDOS_TENANTID;
+const authToken = process.env.STEEDOS_AUTHTOKEN;
 if (Builder.isBrowser){
   (window as any).Builder = Builder;
   (window as any)._ = _;
   Builder.set({ 
-    rootUrl: process.env.STEEDOS_ROOT_URL,
+    rootUrl: rootUrl,
     context: {
-      rootUrl: process.env.STEEDOS_ROOT_URL,
-      userId: process.env.STEEDOS_USERID || localStorage.getItem('steedos:userId'),
-      tenantId: process.env.STEEDOS_TENANTID || localStorage.getItem('steedos:spaceId'),
-      authToken: process.env.STEEDOS_AUTHTOKEN || localStorage.getItem('steedos:token'),
+      rootUrl: rootUrl,
+      userId: userId || localStorage.getItem('steedos:userId'),
+      tenantId: tenantId || localStorage.getItem('steedos:spaceId'),
+      authToken: authToken || localStorage.getItem('steedos:token'),
     } 
   });
 }
@@ -104,6 +109,7 @@ export default {
         ]).then(()=>{
           (window as any).React = (window as any).amisRequire("react");
           (window as any).ReactDOM = (window as any).amisRequire("react-dom");
+          (window as any).lodash = (window as any)._;
           setIsLoaded(true)
         }).catch((error)=>{
           console.error(error)
