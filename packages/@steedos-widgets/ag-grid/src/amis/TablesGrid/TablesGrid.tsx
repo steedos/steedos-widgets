a597aeed0c95c76cee9a7e8a2f1d0478ecc72bf1
殷亮辉 <yinlianghui@hotoa.com>
2025-01-06 20:14:05 +0800

    tables grid组件增加baseId属性

--- packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/TablesGrid.tsx
+++ packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/TablesGrid.tsx
@@ -2,19 +2,16 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-12-25 13:52:44
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-06 17:45:24
+ * @LastEditTime: 2025-01-06 20:11:48
  */
-import React, { useEffect, useState, useRef } from 'react';
-import { keys, pick, difference, pickBy, has, each, isString } from 'lodash';
 import { getTablesGridSchema } from "./tables";
 
 export const AmisTablesGrid = async (props: any) => {
-  const { $schema, data, defaultData, className = "", tableId, mode = "edit", env, style } = props;
+  const { $schema, data, defaultData, className = "", tableId, baseId = "default", mode = "edit", env, style } = props;
   console.log('AmisTablesGrid===', props);
-  // const [tableSchema, setTableSchema] = useState({} as any);
   const amisSchemaData = Object.assign({}, data, defaultData);
   // const appId = data?.appId || defaultData?.appId;
-  let tableSchema = await getTablesGridSchema(tableId, mode, { env });
+  let tableSchema = await getTablesGridSchema(tableId, baseId, mode, { env });
   let amisSchema: any = tableSchema.amisSchema;
   amisSchema.data = Object.assign({}, amisSchema.data, amisSchemaData);
   amisSchema.className = `${amisSchema.className} ${className}`;
--- packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/tables.ts
+++ packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/tables.ts
@@ -2,28 +2,22 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2025-01-02 15:39:40
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-06 16:56:22
+ * @LastEditTime: 2025-01-06 20:09:12
  */
 // import { getMeta, getColumnDef, getGridOptions, getTableHeader } from '../tables';
 import { getColumnDef, getDataTypeDefinitions } from '../AirtableGrid/gridOptions';
 import { getDataSource } from './dataSource';
 
-export const B6_TABLES_BASEID = "default";
-
-const ROOT_URL = "http://127.0.0.1:5800";
 const B6_HOST = "http://localhost:5100";//process.env.B6_HOST || "";
-const B6_TABLES_API = `${B6_HOST}/api/v6/tables`;
-export const B6_TABLES_ROOTURL = `${B6_TABLES_API}/${B6_TABLES_BASEID}`;
 
-export const B6_TABLES_METABASE_ROOTURL = `${B6_TABLES_API}/meta/bases/${B6_TABLES_BASEID}/tables`;
-
-
-async function getMeta(tableId: string, force: boolean = false) {
+async function getMeta(tableId: string, baseId: string = 'default', baseUrl: string = '') {
     if (!tableId) {
         return;
     }
+    const tablesApi = `${baseUrl}/api/v6/tables`;
+    const metaApi = `${tablesApi}/meta/bases/${baseId}/tables`;
     try {
-        const response = await fetch(`${B6_TABLES_METABASE_ROOTURL}/${tableId}`, {
+        const response = await fetch(`${metaApi}/${tableId}`, {
             credentials: 'include',
             // "headers": {
             //     'Content-Type': 'application/json',
@@ -40,12 +34,12 @@ async function getMeta(tableId: string, force: boolean = false) {
 
 export async function getTablesGridSchema(
     tableId: string,
+    baseId: string,
     mode: string, //edit/read/admin
     { env }
 ) {
-    const meta = await getMeta(tableId);
-    const baseUrl = B6_HOST;
-    const baseId = B6_TABLES_BASEID;
+    const baseUrl = B6_HOST;//TODO:要改为从amis data context中取值
+    const meta = await getMeta(tableId, baseId, baseUrl);
     const dataSource = getDataSource({ baseUrl, baseId, tableId});
 
     const getColumnDefs = async ({ dispatchEvent }) => {
