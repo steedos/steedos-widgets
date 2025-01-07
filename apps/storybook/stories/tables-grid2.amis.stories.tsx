12428273a64ea2691a2340aa445a185a2263c4f8
殷亮辉 <yinlianghui@hotoa.com>
2025-01-07 10:34:11 +0800

    先移除无用 storybook

--- apps/storybook/stories/tables-grid2.amis.stories.tsx
+++ apps/storybook/stories/tables-grid2.amis.stories.tsx
@@ -1,102 +1,102 @@
-/*
- * @Author: 殷亮辉 yinlianghui@hotoa.com
- * @Date: 2025-01-02 15:33:22
- * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-02 21:11:49
- */
-import { React, AmisRender } from '../components/AmisRender';
-import { useEffect, useState, useRef } from 'react';
+// /*
+//  * @Author: 殷亮辉 yinlianghui@hotoa.com
+//  * @Date: 2025-01-02 15:33:22
+//  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+//  * @LastEditTime: 2025-01-02 21:11:49
+//  */
+// import { React, AmisRender } from '../components/AmisRender';
+// import { useEffect, useState, useRef } from 'react';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Enterprise/Tables2',
-};
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: 'Enterprise/Tables2',
+// };
 
-const data = {};
+// const data = {};
 
-const env = {
-  assetUrls: [
-    process.env.STEEDOS_UNPKG_URL + '/@steedos-widgets/amis-object/dist/assets.json',
-    process.env.STEEDOS_UNPKG_URL + '/@steedos-widgets/ag-grid/dist/assets.json',
-  ],
-};
+// const env = {
+//   assetUrls: [
+//     process.env.STEEDOS_UNPKG_URL + '/@steedos-widgets/amis-object/dist/assets.json',
+//     process.env.STEEDOS_UNPKG_URL + '/@steedos-widgets/ag-grid/dist/assets.json',
+//   ],
+// };
 
-/** 以上为可复用代码 **/
-export const Gerneral = () => (
-  <AmisRender
-    data={data}
-    env={env}
-    schema={{
-      "type": "page",
-      "body": [
-        {
-          "type": "steedos-tables-grid2",
-          "className": "h-96",
-          "style": {
-            "height": "calc(100vh - 58px)"
-          },
-          "tableId": "67658ac0cc184d0efc68b752",
-          "mode": "admin"
-        }
-      ],
-    }}
-  />
-)
+// /** 以上为可复用代码 **/
+// export const Gerneral = () => (
+//   <AmisRender
+//     data={data}
+//     env={env}
+//     schema={{
+//       "type": "page",
+//       "body": [
+//         {
+//           "type": "steedos-tables-grid2",
+//           "className": "h-96",
+//           "style": {
+//             "height": "calc(100vh - 58px)"
+//           },
+//           "tableId": "67658ac0cc184d0efc68b752",
+//           "mode": "admin"
+//         }
+//       ],
+//     }}
+//   />
+// )
 
-export const SwitchTableId = () => {
-  // const [tableId, setTableId] = useState('675ce746f7d71010aaeb3fe6');
-  return (<>
-    {/* <input type='input' onChange={(e) => {
-      setTableId(e.target.value)
-    }} /> */}
-    <AmisRender
-      data={data}
-      env={env}
-      schema={{
-        "type": "page",
-        "body": [{
-          "type": "service",
-          "id": "service_wrap",
-          "body": [
-            {
-              "name": "text",
-              "type": "input-text",
-              "label": "text",
-              "onEvent": {
-                "change": {
-                  "actions": [
-                    {
-                      "actionType": "setValue",
-                      "componentId": "service_wrap",
-                      "args": {
-                        "value": {
-                          "tableId": "${event.data.value}"
-                        }
-                      }
-                    }
-                  ]
-                }
-              }
-            },
-            {
-              "type": "tpl",
-              "tpl": "TableId: ${tableId}"
-            },
-            {
-              "type": "steedos-tables-grid2",
-              "className": "h-96",
-              "style": {
-                "height": "calc(100vh - 58px)"
-              },
-              "tableId": "${tableId}",
-              "mode": "admin"
-            }
-          ],
-          "data": {
-            "tableId": "675ce746f7d71010aaeb3fe6"
-          }
-        }],
-      }}
-    />
-  </>)
-}
+// export const SwitchTableId = () => {
+//   // const [tableId, setTableId] = useState('675ce746f7d71010aaeb3fe6');
+//   return (<>
+//     {/* <input type='input' onChange={(e) => {
+//       setTableId(e.target.value)
+//     }} /> */}
+//     <AmisRender
+//       data={data}
+//       env={env}
+//       schema={{
+//         "type": "page",
+//         "body": [{
+//           "type": "service",
+//           "id": "service_wrap",
+//           "body": [
+//             {
+//               "name": "text",
+//               "type": "input-text",
+//               "label": "text",
+//               "onEvent": {
+//                 "change": {
+//                   "actions": [
+//                     {
+//                       "actionType": "setValue",
+//                       "componentId": "service_wrap",
+//                       "args": {
+//                         "value": {
+//                           "tableId": "${event.data.value}"
+//                         }
+//                       }
+//                     }
+//                   ]
+//                 }
+//               }
+//             },
+//             {
+//               "type": "tpl",
+//               "tpl": "TableId: ${tableId}"
+//             },
+//             {
+//               "type": "steedos-tables-grid2",
+//               "className": "h-96",
+//               "style": {
+//                 "height": "calc(100vh - 58px)"
+//               },
+//               "tableId": "${tableId}",
+//               "mode": "admin"
+//             }
+//           ],
+//           "data": {
+//             "tableId": "675ce746f7d71010aaeb3fe6"
+//           }
+//         }],
+//       }}
+//     />
+//   </>)
+// }
