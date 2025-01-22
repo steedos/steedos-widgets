de4cae2577a4b40228391a30402d20e13e8bd57b
殷亮辉 <yinlianghui@hotoa.com>
2025-01-22 17:17:10 +0800

    remove debug console log

--- packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/TablesGrid.tsx
+++ packages/@steedos-widgets/ag-grid/src/amis/TablesGrid/TablesGrid.tsx
@@ -2,13 +2,13 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-12-25 13:52:44
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-22 16:57:21
+ * @LastEditTime: 2025-01-22 17:16:38
  */
 import { getTablesGridSchema } from "./tables";
 
 export const AmisTablesGrid = async (props: any) => {
   const { $schema, data, defaultData, className = "", tableId, baseId = "default", mode = "edit", env, style, licenseKey } = props;
-  console.log('AmisTablesGrid===', props);
+  // console.log('AmisTablesGrid===', props);
   const amisSchemaData = Object.assign({}, data, defaultData);
   // const appId = data?.appId || defaultData?.appId;
   let tableSchema = await getTablesGridSchema(tableId, baseId, mode, { env, data, licenseKey });
