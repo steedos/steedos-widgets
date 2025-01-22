f8eb604957cf2d039b6f9522214fca3eaf0e0143
baozhoutao <baozhoutao@hotoa.com>
2025-01-22 16:50:10 +0800

    Update index.tsx

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2025-01-22 12:16:45
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2025-01-22 16:23:00
+ * @LastEditTime: 2025-01-22 16:47:37
  * @Description: 
  */
 import React from "react";
@@ -16,6 +16,16 @@ import ReactDOM from 'react-dom';
 window['React'] = React;
 window['ReactDOM'] = ReactDOM;
 (window as any)['Builder'] = Builder;
+Builder.settings = { 
+  rootUrl: localStorage.getItem('steedos:rootUrl'),
+  assetUrls: ['http://127.0.0.1:8080/@steedos-widgets/amis-object@v6.3.12-beta.15/dist/assets.json'],
+  context: {
+    rootUrl: localStorage.getItem('steedos:rootUrl'),
+    userId: localStorage.getItem('steedos:userId'),
+    tenantId: localStorage.getItem('steedos:spaceId'),
+    authToken: localStorage.getItem('steedos:authToken'),
+  } 
+};
 (window as any)['builder'] = builder;
 (window as any)['BuilderComponent'] = BuilderComponent;
 
