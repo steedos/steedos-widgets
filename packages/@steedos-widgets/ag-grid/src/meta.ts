5f1a9fe9c82276faa2b985892c9164031209a0ab
殷亮辉 <yinlianghui@hotoa.com>
2025-02-11 20:09:50 +0800

    移除无用组件

--- packages/@steedos-widgets/ag-grid/src/meta.ts
+++ packages/@steedos-widgets/ag-grid/src/meta.ts
@@ -2,15 +2,13 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-08-31 16:32:35
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-06 17:51:50
+ * @LastEditTime: 2025-02-11 20:08:26
  * @Description: 
  */
 import AgGrid from "./metas/AgGrid";
 import AmisTablesGrid from "./metas/AmisTablesGrid";
-import AmisTablesGrid_OLD from "./metas/AmisTablesGrid_OLD";
-import AmisTablesGrid3 from "./metas/AmisTablesGrid3";
 import AmisAirtableGrid from "./metas/AmisAirtableGrid";
-const components = [AgGrid, AmisTablesGrid, AmisTablesGrid_OLD, AmisTablesGrid3, AmisAirtableGrid];
+const components = [AgGrid, AmisTablesGrid, AmisAirtableGrid];
 const componentList = [
   {
     title: "华炎魔方",
