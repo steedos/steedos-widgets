49eef0d01f03b281cee862441b4165b72476f625
殷亮辉 <yinlianghui@hotoa.com>
2025-02-11 14:52:15 +0800

    TablesGrid 移除无用代码

--- packages/@steedos-widgets/ag-grid/src/components/index.tsx
+++ packages/@steedos-widgets/ag-grid/src/components/index.tsx
@@ -2,7 +2,6 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-01-18 18:58:37
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-03 15:52:49
+ * @LastEditTime: 2025-02-11 14:51:32
  */
 export * from './AgGrid';
-export * from './TablesGrid';
