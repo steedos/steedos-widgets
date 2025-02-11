2ea92988926721192663e0d60171f3600d08b7ba
殷亮辉 <yinlianghui@hotoa.com>
2025-02-11 09:38:52 +0800

    TablesGrid组件移除无用代码

--- packages/@steedos-widgets/ag-grid/src/amis/index.tsx
+++ packages/@steedos-widgets/ag-grid/src/amis/index.tsx
@@ -2,8 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-12-25 14:56:20
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-01-06 17:45:45
+ * @LastEditTime: 2025-02-11 09:37:58
  */
-export * from './TablesGrid_OLD';
 export * from './TablesGrid';
 export * from './AirtableGrid';
