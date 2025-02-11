48f2bcda1636635a188d5f3c4515bbe15566d6c8
殷亮辉 <yinlianghui@hotoa.com>
2025-02-11 14:29:44 +0800

    TablesGrid 整理storybook，并改成输入框填写tableid属性

--- apps/storybook/stories/tables-grid.amis.stories.tsx
+++ apps/storybook/stories/tables-grid.amis.stories.tsx
@@ -16,7 +16,7 @@ const env = {
 };
 
 /** 以上为可复用代码 **/
-export const Gerneral = () => (
+export const AdminEditRead = () => (
   <AmisRender
     data={data}
     env={env}
@@ -24,13 +24,103 @@ export const Gerneral = () => (
       "type": "page",
       "body": [
         {
-          "type": "steedos-tables-grid",
-          "className": "h-96",
-          "style": {
-            "height": "calc(100vh - 58px)"
-          },
-          "tableId": "67658ac0cc184d0efc68b752",
-          "mode": "admin"
+          "type": "service",
+          "id": "service_wrap",
+          "body": [
+            {
+              "name": "text",
+              "type": "input-text",
+              "label": "TableId",
+              "value": "${tableId}",
+              "onEvent": {
+                "change": {
+                  "actions": [
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${false}"
+                        }
+                      }
+                    },
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${true}",
+                          "tableId": "${event.data.value}"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              "type": "button-group-select",
+              "label": "Mode",
+              "name": "type",
+              "options": [
+                {
+                  "label": "Admin",
+                  "value": "admin"
+                },
+                {
+                  "label": "Edit",
+                  "value": "edit"
+                },
+                {
+                  "label": "Read",
+                  "value": "read"
+                },
+                {
+                  "label": "Unset",
+                  "value": null
+                }
+              ],
+              "onEvent": {
+                "change": {
+                  "actions": [
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${false}"
+                        }
+                      }
+                    },
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${true}",
+                          "tableMode": "${event.data.value}"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              "type": "steedos-tables-grid",
+              "className": "h-96",
+              "style": {
+                "height": "calc(100vh - 108px)"
+              },
+              "tableId": "${tableId}",
+              "mode": "${tableMode}",
+              "visibleOn": "${tableVisible}"
+            }
+          ],
+          "data": {
+            "tableId": "67658ac0cc184d0efc68b752",
+            "tableVisible": "${true}"
+          }
         }
       ],
     }}
@@ -45,14 +135,149 @@ export const Filters = () => (
       "type": "page",
       "body": [
         {
-          "type": "steedos-tables-grid",
-          "className": "h-96",
-          "style": {
-            "height": "calc(100vh - 58px)"
-          },
-          "tableId": "67658ac0cc184d0efc68b752",
-          "mode": "read",
-          "filters": ["int", ">", 100]
+          "type": "service",
+          "id": "service_wrap",
+          "body": [
+            {
+              "name": "tableId",
+              "type": "input-text",
+              "label": "TableId",
+              "value": "${tableId}",
+              "onEvent": {
+                "change": {
+                  "actions": [
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${false}"
+                        }
+                      }
+                    },
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${true}",
+                          "tableId": "${event.data.value}"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              "name": "filters",
+              "type": "input-text",
+              "label": "Filters",
+              "value": "${tableFilters}",
+              "onEvent": {
+                "change": {
+                  "actions": [
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${false}"
+                        }
+                      }
+                    },
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableVisible": "${true}",
+                          "tableFilters": "${event.data.value|toJson}"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              "type": "steedos-tables-grid",
+              "className": "h-96",
+              "style": {
+                "height": "calc(100vh - 108px)"
+              },
+              "tableId": "${tableId}",
+              "mode": "read",
+              "filters": "${tableFilters}",
+              "visibleOn": "${tableVisible}"
+            }
+          ],
+          "data": {
+            "tableId": "67658ac0cc184d0efc68b752",
+            "tableVisible": "${true}",
+            "tableFilters": '${["int", ">", 100]}'
+          }
+        }
+      ],
+    }}
+  />
+)
+export const LicenseKey = () => (
+  <AmisRender
+    data={data}
+    env={env}
+    schema={{
+      "type": "page",
+      "body": [
+        {
+          "type": "service",
+          "id": "service_wrap",
+          "body": [
+            {
+              "name": "text",
+              "type": "input-text",
+              "label": "TableId",
+              "value": "${tableId}",
+              "onEvent": {
+                "change": {
+                  "actions": [
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableId": "${false}"
+                        }
+                      }
+                    },
+                    {
+                      "actionType": "setValue",
+                      "componentId": "service_wrap",
+                      "args": {
+                        "value": {
+                          "tableId": "${event.data.value}"
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              "type": "steedos-tables-grid",
+              "className": "h-96",
+              "style": {
+                "height": "calc(100vh - 108px)"
+              },
+              "tableId": "${tableId}",
+              "mode": "read",
+              "agGridLicenseKey": "1234567890",
+              "visibleOn": "${tableId}"
+            }
+          ],
+          "data": {
+            "tableId": "67658ac0cc184d0efc68b752"
+          }
         }
       ],
     }}
@@ -77,7 +302,8 @@ export const SwitchTableId = () => {
             {
               "name": "text",
               "type": "input-text",
-              "label": "text",
+              "label": "TableId（tableId变化时自动重新加载组件，未实现）",
+              "value": "${tableId}",
               "onEvent": {
                 "change": {
                   "actions": [
@@ -94,10 +320,10 @@ export const SwitchTableId = () => {
                 }
               }
             },
-            {
-              "type": "tpl",
-              "tpl": "TableId: ${tableId}"
-            },
+            // {
+            //   "type": "tpl",
+            //   "tpl": "TableId: ${tableId}"
+            // },
             {
               "type": "steedos-tables-grid",
               "className": "h-96",
@@ -116,25 +342,3 @@ export const SwitchTableId = () => {
     />
   </>)
 }
-
-export const LicenseKey = () => (
-  <AmisRender
-    data={data}
-    env={env}
-    schema={{
-      "type": "page",
-      "body": [
-        {
-          "type": "steedos-tables-grid",
-          "className": "h-96",
-          "style": {
-            "height": "calc(100vh - 58px)"
-          },
-          "tableId": "67658ac0cc184d0efc68b752",
-          "mode": "admin",
-          "agGridLicenseKey": "1234567890"
-        }
-      ],
-    }}
-  />
-)
