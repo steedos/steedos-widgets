ff99ed0a8bf373276f1095240463073e23f2127c
baozhoutao <baozhoutao@hotoa.com>
2025-02-17 15:49:32 +0800

    Update utils.js

--- packages/@steedos-widgets/steedos-lib/src/utils.js
+++ packages/@steedos-widgets/steedos-lib/src/utils.js
@@ -1,14 +1,21 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2023-03-20 16:52:27
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2025-02-17 14:24:43
+ * @Description: 
+ */
 import { isArray, isNil } from 'lodash';
 
 export const safeRunFunction = (fun, args, defaultValue, _this) => {
     try {
-      const Creator = window.Creator;
-      const isCreator = !!(Creator && Creator.getObjectUrl);
-      let regCreator = /\bSteedos\b|\bCreator\b|\bMeteor\b|\bSession\b/;
-      if(!isCreator && regCreator.test(fun)){
-        console.info("调用了Creator|Steedos|Meteor|Session变量的脚本不执行，直接按空值处理。");
-        return "";
-      }
+      // const Creator = window.Creator;
+      // const isCreator = !!(Creator && Creator.getObjectUrl);
+      // let regCreator = /\bSteedos\b|\bCreator\b|\bMeteor\b|\bSession\b/;
+      // if(!isCreator && regCreator.test(fun)){
+      //   console.info("调用了Creator|Steedos|Meteor|Session变量的脚本不执行，直接按空值处理。", fun);
+      //   return "";
+      // }
       // TODO:移除Creator后，上面判断isCreator并返回空的逻辑可以去掉。
       let params = [];
       if(!isNil(args)){
