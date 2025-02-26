c65063f930243082dee638afe7b8a2bfab12ead9
殷亮辉 <yinlianghui@hotoa.com>
2025-02-26 15:05:27 +0800

    ag-grid单元格编辑select multi字段改为调用steedos-field并优化选项值很多时样式

--- packages/@steedos-widgets/ag-grid/src/amis/cellEditor/multiSelect.ts
+++ packages/@steedos-widgets/ag-grid/src/amis/cellEditor/multiSelect.ts
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2025-02-11 17:43:41
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-02-25 15:28:11
+ * @LastEditTime: 2025-02-26 15:02:27
  */
 import { ICellEditorComp, ICellEditorParams, ISelectCellEditorParams } from 'ag-grid-community';
 // import * as amis from 'amis';
@@ -12,22 +12,29 @@ declare const amisRequire: any;
 
 export class AmisMultiSelectCellEditor implements ICellEditorComp {
     private eGui: HTMLElement;
+    private name: string;
     private value: string;
     private amisScope: any;
     private containerId: string;
     private amisSchema: any;
+    private amisData: any;
+    private amisEnv: any;
     private params: ICellEditorParams;
 
     init(params: ICellEditorParams): void {
         this.params = params;
         this.value = params.value;
+        this.amisData = (this.params as any).context.amisData;
+        this.amisEnv = (this.params as any).context.amisEnv;
+        let fieldConfig = (this.params as any).fieldConfig;
+        this.name = fieldConfig.name;
         this.setupGui();
     }
 
     setupGui(): void {
         // 创建编辑器的容器
         this.eGui = document.createElement('div');
-        this.eGui.style.width = '100%';
+        this.eGui.style.width = '200px';
         this.eGui.style.height = '100%';
 
         // 为 amis 组件创建一个唯一的容器 ID
@@ -40,6 +47,8 @@ export class AmisMultiSelectCellEditor implements ICellEditorComp {
         this.eGui.appendChild(containerDiv);
 
         let fieldOptions = (this.params as unknown as ISelectCellEditorParams).values;
+        fieldOptions = fieldOptions && fieldOptions.map(function (n: string) { return { label: n, value: n } }) || [];
+        let fieldConfig = (this.params as any).fieldConfig;
 
         // 定义 amis 的 schema
         this.amisSchema = {
@@ -48,14 +57,23 @@ export class AmisMultiSelectCellEditor implements ICellEditorComp {
             wrapWithPanel: false,
             body: [
                 {
-                    type: 'select',
-                    name: 'cellValue',
-                    value: this.value,
-                    multiple: true,
-                    "options": fieldOptions,
-                    "popOverContainerSelector": `#${this.eGui.id}`
+                    type: 'steedos-field',
+                    // value: this.value,
+                    config: Object.assign({}, fieldConfig, {
+                        label: false,
+                        type: 'select',
+                        multiple: true,
+                        options: fieldOptions,
+                        amis:{
+                            "popOverContainerSelector": `#${this.eGui.id}`,
+                            // valuesNoWrap: true
+                        }
+                    })
                 }
-            ]
+            ],
+            data: {
+                [this.name]: this.value
+            }
         };
 
         // // 渲染 amis 组件
@@ -73,13 +91,13 @@ export class AmisMultiSelectCellEditor implements ICellEditorComp {
         // 在元素被附加到 DOM 后，再调用 amis.embed
         const amis = amisRequire("amis/embed");
         const env = (window as any).BuilderAmisObject.AmisLib.getEvn();
-        this.amisScope = amis.embed(`#${this.containerId}`, this.amisSchema, {}, env);
+        this.amisScope = amis.embed(`#${this.containerId}`, this.amisSchema, { data: this.amisData }, env);
     }
 
     getValue(): any {
-        // 从 amis 中获取当前数据
+        // // 从 amis 中获取当前数据
         const data = this.amisScope.getComponentById('cellForm')?.getValues();
-        return data?.cellValue;
+        return (data || {})[this.name];
     }
 
     destroy?(): void {
