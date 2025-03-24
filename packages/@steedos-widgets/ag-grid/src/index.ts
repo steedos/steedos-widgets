3e3d244f54a16bbfe10d3c18023df9f0f1ebce3a
殷亮辉 <yinlianghui@hotoa.com>
2025-03-24 09:51:38 +0800

    fix(ag-grid):ag-grid升级到33后Theme皮肤失效了

--- packages/@steedos-widgets/ag-grid/src/index.ts
+++ packages/@steedos-widgets/ag-grid/src/index.ts
@@ -2,11 +2,13 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2024-01-18 18:58:37
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2024-12-25 15:21:34
+ * @LastEditTime: 2025-03-21 11:51:55
  */
 export * from './components';
 export * from './amis';
 
 
-import "ag-grid-community/styles/ag-grid.css"; // Core CSS
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+// 升级到ag-grid 33版本后不允许通过 imported as CSS files in our NPM modules 来指定皮肤
+// 默认皮肤就是 ag-theme-quartz 不需要也不允许额外引入，如果回退到32版本需要重新放开以下引用
+// import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+// import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
