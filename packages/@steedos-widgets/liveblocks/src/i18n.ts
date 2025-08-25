967ef0b36fc9cea7be8fd52acfa449f20736f14d
殷亮辉 <yinlianghui@hotoa.com>
2025-08-25 23:21:42 +0800

    fix: liveblocks英文国际化坏了
    https://github.com/steedos/steedos-platform/issues/7402

--- packages/@steedos-widgets/liveblocks/src/i18n.ts
+++ packages/@steedos-widgets/liveblocks/src/i18n.ts
@@ -2,14 +2,14 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2025-07-07 09:46:39
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-07-07 10:58:19
+ * @LastEditTime: 2025-08-25 23:00:02
  */
 import en_us from "./locales/en-US.json"
 import zh_cn from "./locales/zh-CN.json"
 import { i18next } from '@steedos-widgets/amis-lib';
 
 
-i18next.addResources('en-US', 'liveblocks', en_us);
+i18next.addResources('en', 'liveblocks', en_us);
 i18next.addResources('zh-CN', 'liveblocks', zh_cn);
 
 export { i18next };
