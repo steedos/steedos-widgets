c7192f15f8e8fe3a89bae34618aa9b43222a780c
baozhoutao <baozhoutao@hotoa.com>
2025-08-29 15:03:29 +0800

    Update AmisSteedosFlowForm.tsx

--- packages/@steedos-widgets/amis-object/src/amis/AmisSteedosFlowForm.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisSteedosFlowForm.tsx
@@ -1,9 +1,4 @@
 export const AmisSteedosFlowForm = async (props = {className: null, body: null}) => {
-    console.log('AmisSteedosFlowForm===>', {
-      type: "container",
-      className: props.className,
-      body: props.body || []
-    });
     return {
       type: "container",
       className: props.className,
