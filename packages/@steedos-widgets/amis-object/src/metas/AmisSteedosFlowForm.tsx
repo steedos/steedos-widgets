e00f702dcb09345fe752c3ac09508600b952e138
baozhoutao <baozhoutao@hotoa.com>
2025-09-01 13:52:41 +0800

    fix: 流程表单组件国际化

--- packages/@steedos-widgets/amis-object/src/metas/AmisSteedosFlowForm.tsx
+++ packages/@steedos-widgets/amis-object/src/metas/AmisSteedosFlowForm.tsx
@@ -2,13 +2,16 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2025-08-19 15:02:47
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-08-29 20:48:20
+ * @LastEditTime: 2025-09-01 13:00:00
  */
+
+const t = (window as any).steedosI18next.t;
+
 const config: any = {
     componentType: 'amisSchema', 
-    group: "华炎魔方-审批王",
+    group: t('widgets-meta:steedos-flow-form_group', 'Steedos-审批王'),
     componentName: "AmisSteedosFlowForm",
-    title: "流程表单",
+    title: t('widgets-meta:steedos-flow-form_title', '流程表单'),
     docUrl: "",
     screenshot: "",
     npm: {
@@ -19,18 +22,16 @@ const config: any = {
       destructuring: true,
       subName: ""
     },
-    preview: {
-    },
+    preview: {},
     targets: ["steedos__RecordPage", "steedos__AppPage", "steedos__HomePage"],
     engines: ["amis"],
-    // settings for amis.
     amis: {
       name: 'steedos-flow-form',
       icon: "fa-fw fa fa-list-alt"
     }
   };
   
-  export default {
+export default {
     ...config,
     snippets: [
       {
@@ -55,64 +56,59 @@ const config: any = {
         name: config.title,
         description: config.title,
         tags: [config.group],
-        
         isBaseComponent: true,
         order: -10000,
         icon: config.amis.icon,
         scaffold: {
-            type: 'steedos-flow-form',
-          body: [],// 容器类字段
+          type: 'steedos-flow-form',
+          body: [],
         },
-        // 容器类组件必需字段
         regions: [
           {
             key: 'body',
-            label: '内容区'
+            label: t('widgets-meta:steedos-flow-form_region_body', '内容区')
           },
         ],
         previewSchema: {
           type: config.amis.name,
         },
-        panelTitle: "设置",
+        panelTitle: t('widgets-meta:steedos-flow-form_panelTitle', '设置'),
         panelControls: [
           {
             type: 'text',
             name: 'name',
-            label: '表单名',
-            "validateOnChange": true,
-            "validations": {
-                "isVariableName": /^[a-zA-Z]([A-Za-z0-9]|_(?!_))*[A-Za-z0-9]$/
+            label: t('widgets-meta:steedos-flow-form_name', '表单名'),
+            validateOnChange: true,
+            validations: {
+                isVariableName: /^[a-zA-Z]([A-Za-z0-9]|_(?!_))*[A-Za-z0-9]$/
             }
           },
           {
             type: 'radios',
             name: 'style',
-            label: '样式',
+            label: t('widgets-meta:steedos-flow-form_style', '样式'),
             options: [
-              {label: '表格', value: 'table'},
-              // {label: '分组', value: 'groups'},
-              // {label: '选项卡', value: 'tabs'},
-              {label: '向导', value: 'wizard'}
+              {label: t('widgets-meta:steedos-flow-form_style_table', '表格'), value: 'table'},
+              {label: t('widgets-meta:steedos-flow-form_style_wizard', '向导'), value: 'wizard'}
             ]
           },
           {
             type: 'radios',
             name: 'mode',
-            label: '模式',
+            label: t('widgets-meta:steedos-flow-form_mode', '模式'),
             visibleOn: "${style === 'wizard'}",
             options: [
-              {label: '默认', value: 'normal'},
-              {label: '水平模式', value: 'horizontal'},
-              {label: '内联模式', value: 'inline'}
+              {label: t('widgets-meta:steedos-flow-form_mode_normal', '默认'), value: 'normal'},
+              {label: t('widgets-meta:steedos-flow-form_mode_horizontal', '水平模式'), value: 'horizontal'},
+              {label: t('widgets-meta:steedos-flow-form_mode_inline', '内联模式'), value: 'inline'}
             ]
           },
           {
             type: 'textarea',
             name: 'description',
-            label: '描述'
+            label: t('widgets-meta:steedos-flow-form_description', '描述')
           }
         ]
       }
     }
-  };
-  
+};
