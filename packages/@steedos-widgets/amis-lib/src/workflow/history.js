abad02d13c72acd717a9a158afa022d5a3682190
殷亮辉 <yinlianghui@hotoa.com>
2025-11-03 10:28:33 +0800

    fix: 签批历程文字颜色样式未正常生效

--- packages/@steedos-widgets/amis-lib/src/workflow/history.js
+++ packages/@steedos-widgets/amis-lib/src/workflow/history.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-24 16:48:28
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-08-28 01:38:39
+ * @LastEditTime: 2025-11-03 10:23:32
  * @Description: 
  */
 
@@ -34,14 +34,14 @@ export const getInstanceApprovalHistory = async ()=>{
           {
             "name": "finish_date",
             "label": "finish_date",
-            "classNameExpr": "<%= data.finish_date == i18next.t('frontend_workflow_approval_history_read') ? 'text-[blue]' : (data.finish_date == i18next.t('frontend_workflow_approval_history_unprocessed') ? 'text-[red]' : '') %>"
+            "classNameExpr": `<%= data.finish_date == '${i18next.t('frontend_workflow_approval_history_read')}' ? 'text-[blue]' : (data.finish_date == '${i18next.t('frontend_workflow_approval_history_unprocessed')}' ? 'text-[red]' : '') %>`
             // "type": "datetime",
             // "format": "YYYY-MM-DD HH:mm"
           },
           {
             "name": "judge",
             "label": "judge",
-            "classNameExpr": "<%= data.judge == i18next.t('frontend_workflow_approval_judge_approved') ? 'text-green-600' : (data.judge == i18next.t('frontend_workflow_approval_judge_rejected') ? 'text-[red]' : '') %>"
+            "classNameExpr": `<%= data.judge == '${i18next.t('frontend_workflow_approval_judge_approved')}' ? 'text-green-600' : (data.judge == '${i18next.t('frontend_workflow_approval_judge_rejected')}' ? 'text-[red]' : '') %>`
           }
         ]
       }
