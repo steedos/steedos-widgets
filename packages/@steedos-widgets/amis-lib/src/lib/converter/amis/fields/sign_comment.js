85a96b9d0996352682d4e4c901af8a64022b312b
殷亮辉 <yinlianghui@hotoa.com>
2025-11-06 10:02:30 +0800

    fix: 签字意见处理人显示不对，设计器中未能显示签字意见字段
    https://github.com/steedos/steedos-plugins/issues/22

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/sign_comment.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/fields/sign_comment.js
@@ -2,9 +2,10 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2025-11-04 12:00:23
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-11-05 23:01:39
+ * @LastEditTime: 2025-11-06 10:00:42
  */
 import * as _ from 'lodash'
+
 export function signCommentToAmis(field, readonly, ctx) {
     if (!ctx) {
         ctx = {};
@@ -13,12 +14,12 @@ export function signCommentToAmis(field, readonly, ctx) {
     let signComment = `
         '<div class="instance-sign-item' +  (item.is_finished ? ' mt-1' : ' text-gray-500 border-l-2 px-1 border-blue-500 my-approve not-finished') + '">' + 
             '<p class="m-0 p-0">' + item.description + '</p>' + 
-            '&emsp;&emsp;' + global.user.name + 
+            '&emsp;&emsp;' + item.handler_name + 
         '</div>'`;
     let html = `
         \${JOIN(ARRAYMAP(record.signCommentFields["${field.name}"].comments, item => ${signComment}), '')}
     `;
-    let amisSchema = {
+    let signCommentSchema = {
         "type": "wrapper",
         "className": "instance-sign",
         "body": [
@@ -26,10 +27,15 @@ export function signCommentToAmis(field, readonly, ctx) {
                 "type": "tpl",
                 "tpl": html,
                 "className": "instance-sign-content block m-2.5"
-            }
+            },
             // TODO:签批链接，点击弹出填写签批意见，readonly时不显示签批链接
         ],
         "size": "none"
+    };
+    let amisSchema = {
+        "type": "control",
+        "label": field.label,
+        "body": [signCommentSchema]
     };
     return amisSchema;
 }
