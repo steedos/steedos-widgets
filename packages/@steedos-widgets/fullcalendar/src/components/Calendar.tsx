1bf3a26027a1c41a411dc02b9e7e87d52594c962
殷亮辉 <yinlianghui@hotoa.com>
2025-11-27 22:32:17 +0800

    日历控件恢复到原来的5.11.4版本，不升级到最新版本，稳定为主

--- packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
+++ packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2023-03-22 09:31:21
- * @LastEditors: yinlianghui yinlianghui@hotoa.com
- * @LastEditTime: 2025-11-27 17:30:58
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2025-11-27 22:28:40
  * @FilePath: /steedos-widgets/packages/@steedos-widgets/fullcalendar/src/components/Calendar.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import listPlugin from '@fullcalendar/list';
 // import multimonthPlugin from '@fullcalendar/multimonth';
-import resourcePlugin from '@fullcalendar/resource';
+import resourcePlugin from '@fullcalendar/resource-common';
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 import allLocales from '@fullcalendar/core/locales-all';
 import './Calendar.css';
