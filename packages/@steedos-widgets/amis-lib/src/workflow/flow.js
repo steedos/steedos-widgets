10cfc46078389abb827185f1bb2ac6bf4e26038e
yinlianghui <yinlianghui@hotoa.com>
2025-12-09 16:34:49 +0800

    fix: 申请单垂直滚动条应该显示在最右侧，而不是限制在宽度1024px的位置
    https://github.com/steedos/steedos-plugins/issues/27

--- packages/@steedos-widgets/amis-lib/src/workflow/flow.js
+++ packages/@steedos-widgets/amis-lib/src/workflow/flow.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-07 16:20:45
  * @LastEditors: yinlianghui yinlianghui@hotoa.com
- * @LastEditTime: 2025-12-09 16:06:24
+ * @LastEditTime: 2025-12-09 16:26:53
  * @Description:
  */
 import {
@@ -1107,7 +1107,7 @@ export const getFlowFormSchema = async (instance, box) => {
         "font-size": "14px",
         "font-weight": "500"
       },
-      ".steedos-amis-instance-view.steedos-instance-style-table .antd-Page-body": {
+      ".steedos-amis-instance-view.steedos-instance-style-table .antd-Page-body .steedos-amis-instance-view-content": {
         "width": "1024px"
       },
       ".antd-List-placeholder": {
@@ -1127,66 +1127,71 @@ export const getFlowFormSchema = async (instance, box) => {
         "background": "transparent !important"
       }
     },
-    body: [
-      await getAttachments(instance),
-      await getRelatedInstances(instance),
-      await getRelatedRecords(instance),
-      {
-        type: "form",
-        debug: false,
-        wrapWithPanel: false,
-        resetAfterSubmit: true,
-        body: [
-          {
-            type: "tpl",
-            id: "u:f5bb0ad602a6",
-            tpl: `<div class="instance-name">${instance.title}</div>`,
-            inline: true,
-            wrapperComponent: "",
-            style: {
-              fontFamily: "",
-              fontSize: 12,
-              textAlign: "center",
-            },
-          },
-          formContentSchema,
-          await getApplicantTableView(instance),
-        ],
-        id: "instance_form",
-        onEvent: {
-          // validateError: {
-          //   weight: 0,
-          //   actions: [
-          //     {
-          //       "componentId": "",
-          //       "args": {
-          //         "msgType": "info",
-          //         "position": "top-right",
-          //         "closeButton": true,
-          //         "showIcon": true,
-          //         "title": i18next.t('frontend_workflow_submit_validate_error_title'),//"提交失败",
-          //         "msg": i18next.t('frontend_workflow_submit_validate_error_msg'),//"请填写必填字段"
-          //       },
-          //       "actionType": "toast"
-          //     }
-          //   ],
-          // },
-          change: {
-            weight: 0,
-            actions: [
-              {
-                "actionType": "reload",
-                "componentId": "u:next_step",
-                "args": {}
+    body: [{
+      "type": "wrapper",
+      "body": [
+        await getAttachments(instance),
+        await getRelatedInstances(instance),
+        await getRelatedRecords(instance),
+        {
+          type: "form",
+          debug: false,
+          wrapWithPanel: false,
+          resetAfterSubmit: true,
+          body: [
+            {
+              type: "tpl",
+              id: "u:f5bb0ad602a6",
+              tpl: `<div class="instance-name">${instance.title}</div>`,
+              inline: true,
+              wrapperComponent: "",
+              style: {
+                fontFamily: "",
+                fontSize: 12,
+                textAlign: "center",
               },
-              ...changeEvents
-            ]
+            },
+            formContentSchema,
+            await getApplicantTableView(instance),
+          ],
+          id: "instance_form",
+          onEvent: {
+            // validateError: {
+            //   weight: 0,
+            //   actions: [
+            //     {
+            //       "componentId": "",
+            //       "args": {
+            //         "msgType": "info",
+            //         "position": "top-right",
+            //         "closeButton": true,
+            //         "showIcon": true,
+            //         "title": i18next.t('frontend_workflow_submit_validate_error_title'),//"提交失败",
+            //         "msg": i18next.t('frontend_workflow_submit_validate_error_msg'),//"请填写必填字段"
+            //       },
+            //       "actionType": "toast"
+            //     }
+            //   ],
+            // },
+            change: {
+              weight: 0,
+              actions: [
+                {
+                  "actionType": "reload",
+                  "componentId": "u:next_step",
+                  "args": {}
+                },
+                ...changeEvents
+              ]
+            }
           }
-        }
-      },
-      await getInstanceApprovalHistory(),
-      await getApproveButton(instance, { submitEvents , nextStepInitedEvents, nextStepChangeEvents, nextStepUserChangeEvents})
-    ],
+        },
+        await getInstanceApprovalHistory(),
+        await getApproveButton(instance, { submitEvents , nextStepInitedEvents, nextStepChangeEvents, nextStepUserChangeEvents})
+      ],
+      "size": "none",
+      "className": "steedos-amis-instance-view-content"
+    }],
     id: "u:instancePage",
     messages: {},
     pullRefresh: {},
