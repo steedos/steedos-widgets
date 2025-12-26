e4b9b1844ed3a91d42dce02a171ae9ed58bb0864
yinlianghui <yinlianghui@hotoa.com>
2025-12-26 17:51:30 +0800

    fix: 分栏模式记录详细页面设置最大高度以解决 crud autofill 在此复杂页面上刷新浏览器时不触发高度计算问题
    https://github.com/steedos/steedos-platform/issues/8245

--- packages/@steedos-widgets/amis-object/src/pages/PageObject.tsx
+++ packages/@steedos-widgets/amis-object/src/pages/PageObject.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-04-12 15:00:42
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2024-01-16 10:32:11
+ * @LastEditors: yinlianghui yinlianghui@hotoa.com
+ * @LastEditTime: 2025-12-26 17:47:28
  * @Description: 
  */
 import './PageObject.less';
@@ -38,6 +38,8 @@ export const PageObject = async (props) => {
                     "className": {
                         "p-0 flex-shrink-0 min-w-[388px] w-fit lg:order-first lg:flex lg:flex-col": "${display == 'split'}",
                         'h-full': "${display != 'split'}",
+                        //分栏模式记录详细页面设置最大高度以解决 crud autofill 在此复杂页面上刷新浏览器时不触发高度计算问题
+                        "steedos-listview-split-max-height": "${pageType === 'record' && display == 'split'}",
                     },
                     "body": {
                         "name": `${ data.objectName}-listview-${ data.listViewId}`,
