82a84590b479e51db27029096ef6871aadd64a9a
包周涛 <baozhoutao@hotoa.com>
2026-04-20 20:51:48 +0800

    Propagate displayAs and fix header spacing

--- packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
+++ packages/@steedos-widgets/amis-lib/src/lib/converter/amis/toolbar.js
@@ -221,7 +221,7 @@ function getObjectHeaderQuickSearchBox(mainObject, fields, formFactor, { isLooku
 
 export function getObjectHeaderToolbar(mainObject, fields, formFactor, { 
   showDisplayAs = false, hiddenCount = false, headerToolbarItems,
-  filterVisible = true, isLookup = false, keywordsSearchBoxName } = {}){
+  filterVisible = true, isLookup = false, keywordsSearchBoxName, displayAs } = {}){
   // console.log(`getObjectHeaderToolbar====>`, filterVisible)
   // console.log(`getObjectHeaderToolbar`, mainObject)
 
@@ -261,7 +261,7 @@ export function getObjectHeaderToolbar(mainObject, fields, formFactor, {
     //TODO: dropdown-button只支持在按钮上方配置提示，对于上方按钮的点击会有影响，为保持统一，暂时去除，等待amis优化，https://github.com/baidu/amis/issues/7330
     // "tooltip": i18next.t('frontend_button_reload_tooltip'),
     "tooltipPlacement": "top",
-    "className": `bg-white p-2 rounded !text-gray-500 list-view-btn-reload ${formFactor === 'SMALL' ? 'hidden' : ''}`,
+    "className": `bg-white p-2 rounded !text-gray-500 list-view-btn-reload`,
     "label": "",
     "icon": "fa fa-sync",
     // "visibleOn": "${!showFieldsFilter}",
