a327580e91236b43227d2b9e3006e68a05ac0c4b
包周涛 <baozhoutao@hotoa.com>
2026-05-06 13:46:48 +0800

    Update AmisObjectForm.tsx

--- packages/@steedos-widgets/amis-object/src/amis/AmisObjectForm.tsx
+++ packages/@steedos-widgets/amis-object/src/amis/AmisObjectForm.tsx
@@ -101,6 +101,7 @@ export const AmisObjectForm = async (props) => {
   if(has(props, 'objectApiName')){
     amisSchema.data.objectName = objectApiName;
   }
+  // console.log("===AmisObjectForm=amisSchema==", amisSchema);
 
   return amisSchema;
 }
